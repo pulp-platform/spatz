1e2fa5d83b127bb56d2bb28965c160281e8ba79d
Domenic Wüthrich <domenicw@student.ethz.ch>
2021-11-12 13:04:54 +0100

    [hardware] Add consistent vrf output dummy signals

--- hardware/src/spatz_vrf.sv
+++ hardware/src/spatz_vrf.sv
@@ -5,7 +5,6 @@
 // Author: Domenic Wüthrich, ETH Zurich
 
 module spatz_vrf import spatz_pkg::*; #(
-  parameter int unsigned DATA_WIDTH = 32,
   parameter int unsigned NR_READ_PORTS = 0,
   parameter int unsigned NR_WRITE_PORTS = 0
 ) (
@@ -22,5 +21,12 @@ module spatz_vrf import spatz_pkg::*; #(
   output vreg_data_t [NR_READ_PORTS-1:0] rdata_o,
   output logic       [NR_READ_PORTS-1:0] rvalid_o
 );
+
+  // Write dummy out
+  assign wvalid_o = '1;
+
+  // Read dummy in
+  assign rdata_o  = '0;
+  assign rvalid_o = '1;
 
 endmodule : spatz_vrf
