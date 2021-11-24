43c7169d060a2f487a4fadc8e0a5684d5c2552ca
Domenic Wüthrich <domenicw@student.ethz.ch>
2021-11-24 09:23:56 +0100

    [hardware] Change spatz request handling in controller

--- hardware/src/spatz_controller.sv
+++ hardware/src/spatz_controller.sv
@@ -42,6 +42,7 @@ module spatz_controller
   // Spatz request
   spatz_req_t spatz_req;
   logic       spatz_req_valid;
+  logic       spatz_req_illegal;
 
   //////////
   // CSRs //
@@ -64,22 +65,22 @@ module spatz_controller
     if (spatz_req_valid) begin
 
       // Reset vstart to zero
-      if (decoder_rsp.spatz_req.op_cgf.reset_vstart) begin
+      if (spatz_req.op_cgf.reset_vstart) begin
         vstart_d = '0;
       end
 
       // Set new vstart when written over vcsrs
-      if (decoder_rsp.spatz_req.op == VCSR) begin
-        if (decoder_rsp.spatz_req.op_cgf.write_vstart) begin
-          vstart_d = vlen_t'(decoder_rsp.spatz_req.rs1);
-        end else if (decoder_rsp.spatz_req.op_cgf.set_vstart) begin
-          vstart_d = vstart_q | vlen_t'(decoder_rsp.spatz_req.rs1);
-        end else if (decoder_rsp.spatz_req.op_cgf.clear_vstart) begin
-          vstart_d = vstart_q & ~vlen_t'(decoder_rsp.spatz_req.rs1);
+      if (spatz_req.op == VCSR) begin
+        if (spatz_req.op_cgf.write_vstart) begin
+          vstart_d = vlen_t'(spatz_req.rs1);
+        end else if (spatz_req.op_cgf.set_vstart) begin
+          vstart_d = vstart_q | vlen_t'(spatz_req.rs1);
+        end else if (spatz_req.op_cgf.clear_vstart) begin
+          vstart_d = vstart_q & ~vlen_t'(spatz_req.rs1);
         end
       end
 
-      if (decoder_rsp.spatz_req.op == VCFG) begin
+      if (spatz_req.op == VCFG) begin
         // Check if vtype is valid
         if ((vtype_d.vsew > EW_32) || (vtype_d.vlmul == LMUL_RES) || (vtype_d.vlmul == LMUL_F8) || (signed'(vtype_d.vlmul) + signed'($clog2(ELENB)) < signed'(vtype_d.vsew))) begin
           // Invalid
@@ -89,13 +90,13 @@ module spatz_controller
           // Valid
 
           // Set new vtype
-          vtype_d = decoder_rsp.spatz_req.vtype;
-          if (~decoder_rsp.spatz_req.op_cgf.keep_vl) begin
+          vtype_d = spatz_req.vtype;
+          if (!spatz_req.op_cgf.keep_vl) begin
             // Normal stripmining mode or set to MAXVL
             automatic int unsigned vlmax = 0;
-            vlmax = VLENB >> decoder_rsp.spatz_req.vtype.vsew;
+            vlmax = VLENB >> spatz_req.vtype.vsew;
 
-            unique case (decoder_rsp.spatz_req.vtype.vlmul)
+            unique case (spatz_req.vtype.vlmul)
               LMUL_F2: vlmax >>= 1;
               LMUL_F4: vlmax >>= 2;
               LMUL_1: vlmax <<= 0;
@@ -104,18 +105,18 @@ module spatz_controller
               LMUL_8: vlmax <<= 3;
             endcase
 
-            vl_d = (decoder_rsp.spatz_req.rs1 == '1) ? MAXVL : (vlmax < decoder_rsp.spatz_req.rs1) ? vlmax : decoder_rsp.spatz_req.rs1;
+            vl_d = (spatz_req.rs1 == '1) ? MAXVL : (vlmax < spatz_req.rs1) ? vlmax : spatz_req.rs1;
           end else begin
             // Keep vl mode
 
             // If new vtype changes ration, mark as illegal
-            if (($signed(decoder_rsp.spatz_req.vtype.vsew) - $signed(vtype_q.vsew)) != ($signed(decoder_rsp.spatz_req.vtype.vlmul) - $signed(vtype_q.vlmul))) begin
+            if (($signed(spatz_req.vtype.vsew) - $signed(vtype_q.vsew)) != ($signed(spatz_req.vtype.vlmul) - $signed(vtype_q.vlmul))) begin
               vtype_d = '{vill: 1'b1, default: '0};
               vl_d    = '0;
             end
           end
         end
-      end // decoder_rsp.spatz_req.op == VCFG
+      end // spatz_req.op == VCFG
     end // spatz_req_valid
   end
 
@@ -123,15 +124,10 @@ module spatz_controller
   // Scoreboard //
   ////////////////
 
-  typedef struct packed {
-    ex_unit_e write;
-    ex_unit_e read;
-  } sb_t;
-
   // Scoreboard keeping track of which execution unit is
   // accessing which register.
   // CON = None
-  sb_t [NRVREG-1:0][1:0] sb_q, sb_d;
+  logic [NRVREG-1:0] sb_q, sb_d;
   `FF(sb_q, sb_d, '0)
 
   //////////////
@@ -172,22 +168,13 @@ module spatz_controller
     end
   end // proc_decode
 
-  /////////////
-  // Issuing //
-  /////////////
-
-  logic retire_csr;
+  ////////////////////
+  // Request Buffer //
+  ////////////////////
 
   // Spatz request buffer
   spatz_req_t spatz_req_buffer_q, spatz_req_buffer_d;
   logic       spatz_req_buffer_empty_q, spatz_req_buffer_empty_d;
-
-  logic stall, vfu_stall, vlsu_stall, vsld_stall;
-  assign stall = vfu_stall || vlsu_stall || vsld_stall;
-  assign vfu_stall = !vfu_req_ready_i && ((decoder_rsp_valid && (decoder_rsp.spatz_req.ex_unit == VFU))
-                     || (!spatz_req_buffer_empty_q && (spatz_req_buffer_q.ex_unit == VFU)));
-  assign vlsu_stall = 1'b0;
-  assign vsld_stall = 1'b0;
 
   // Spatz request buffer is used when there is a new request but execution
   // unit is still busy
@@ -201,11 +188,29 @@ module spatz_controller
     end
   end
 
+  /////////////
+  // Issuing //
+  /////////////
+
+  logic retire_csr;
+
+  logic operands_ready, destination_ready;
+  assign operands_ready = (decoder_rsp_valid | ~spatz_req_buffer_empty_q) & (spatz_req.use_vs1 & ~sb_q[spatz_req.vs1]);
+  assign destination_ready = (decoder_rsp_valid | ~spatz_req_buffer_empty_q) & (~spatz_req.use_vd | (spatz_req.use_vd & ~sb_q[spatz_req.vd]));
+
+  logic stall, vfu_stall, vlsu_stall, vsld_stall;
+  assign stall = vfu_stall | vlsu_stall | vsld_stall;
+  assign vfu_stall = ~vfu_req_ready_i & ((decoder_rsp_valid & (spatz_req.ex_unit == VFU))
+                     | (~spatz_req_buffer_empty_q & (spatz_req_buffer_q.ex_unit == VFU)));
+  assign vlsu_stall = 1'b0;
+  assign vsld_stall = 1'b0;
+
   always_comb begin : proc_issue
     retire_csr = 1'b0;
     spatz_ready_d = spatz_ready_q;
     spatz_req = spatz_req_buffer_empty_q ? decoder_rsp.spatz_req : spatz_req_buffer_q;
     spatz_req.id = x_issue_req_i.id;
+    spatz_req_illegal = decoder_rsp_valid ? decoder_rsp.instr_illegal : 1'b0;
     spatz_req_valid = 1'b0;
 
     spatz_req_buffer_d       = spatz_req_buffer_q;
@@ -217,7 +222,7 @@ module spatz_controller
       spatz_req_buffer_empty_d = 1'b0;
       spatz_ready_d = 1'b0;
     // New decoded instruction if valid
-    end else if ((decoder_rsp_valid && ~decoder_rsp.instr_illegal) || (!spatz_req_buffer_empty_q && !stall)) begin
+    end else if ((decoder_rsp_valid && ~spatz_req_illegal) || (!spatz_req_buffer_empty_q && !stall)) begin
       // Resolve and reset buffer
       if (!spatz_req_buffer_empty_q) begin
         spatz_req = spatz_req_buffer_q;
@@ -242,10 +247,10 @@ module spatz_controller
   always_comb begin : proc_x_issue_resp
     x_issue_resp_o = '0;
 
-    if (decoder_rsp_valid && !decoder_rsp.instr_illegal) begin
+    if (decoder_rsp_valid && !spatz_req_illegal) begin
       x_issue_resp_o.accept = 1'b1;
 
-      case (decoder_rsp.spatz_req.ex_unit)
+      case (spatz_req.ex_unit)
         CON: begin
           x_issue_resp_o.writeback = spatz_req.use_rd;
         end // CON
@@ -270,7 +275,7 @@ module spatz_controller
           end
         end // SLD
       endcase // Operation type
-    end else if (decoder_rsp_valid && decoder_rsp.instr_illegal) begin
+    end else if (decoder_rsp_valid && spatz_req_illegal) begin
       x_issue_resp_o.accept = 1'b0;
     end
   end // proc_x_issue_resp
@@ -285,9 +290,9 @@ module spatz_controller
 
     if (retire_csr) begin
       // Read CSR and write back to cpu
-      if (decoder_rsp.spatz_req.op == VCSR) begin
-        if (decoder_rsp.spatz_req.use_rd) begin
-          unique case (decoder_rsp.spatz_req.op_csr.addr)
+      if (spatz_req.op == VCSR) begin
+        if (spatz_req.use_rd) begin
+          unique case (spatz_req.op_csr.addr)
             riscv_instr::CSR_VSTART: x_result_o.data = elen_t'(vstart_q);
             riscv_instr::CSR_VL:     x_result_o.data = elen_t'(vl_q);
             riscv_instr::CSR_VTYPE:  x_result_o.data = elen_t'(vtype_q);
