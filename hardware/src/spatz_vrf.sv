5498f6187f6d2c2fdb9c4dd0238e80f4eff6af6d
Domenic Wüthrich <domenicw@student.ethz.ch>
2021-11-24 17:21:38 +0100

    [hardware] Enable write byte in vregfile

--- hardware/src/spatz_vrf.sv
+++ hardware/src/spatz_vrf.sv
@@ -36,6 +36,7 @@ module spatz_vrf import spatz_pkg::*; #(
       .NR_READ_PORTS (NR_READ_PORTS),
       .NR_WRITE_PORTS(NR_WRITE_PORTS),
       .NR_REGS       (NRVREG),
+      .ENABLE_WBE    (1),
       .REG_WIDTH     (VLEN/NR_BANKS),
       .ELEM_WIDTH    (N_IPU*ELEN),
       .VADDR_WIDTH   ($bits(vreg_addr_t))
@@ -57,6 +58,7 @@ module vregfile #(
   parameter int unsigned NR_READ_PORTS  = 0,
   parameter int unsigned NR_WRITE_PORTS = 0,
   parameter int unsigned NR_REGS        = 32,
+  parameter logic        ENABLE_WBE     = 1,
   parameter int unsigned REG_WIDTH      = 32,
   parameter int unsigned ELEM_WIDTH     = 32,
   parameter int unsigned VADDR_WIDTH    = 5
@@ -74,20 +76,24 @@ module vregfile #(
 );
 
   localparam int unsigned NUM_ELEM_PER_REG = REG_WIDTH/ELEM_WIDTH;
+  localparam int unsigned ELEM_WBE = ENABLE_WBE ? ELEM_WIDTH/8 : 1;
+  localparam int unsigned ELEM_WBE_SIZE = ENABLE_WBE ? 8 : ELEM_WIDTH;
 
   logic clk;
-  logic [NR_REGS-1:0][NUM_ELEM_PER_REG-1:0] mem_clocks;
+  logic [NR_REGS-1:0][NUM_ELEM_PER_REG-1:0][ELEM_WBE-1:0] mem_clocks;
 
   logic [NR_REGS-1:0][NUM_ELEM_PER_REG-1:0][ELEM_WIDTH-1:0] mem;
 
-  logic [NR_WRITE_PORTS-1:0][ELEM_WIDTH-1:0]                    wdata_q;
-  logic [NR_WRITE_PORTS-1:0][NR_REGS-1:0][NUM_ELEM_PER_REG-1:0] waddr_onehot;
-  logic [NR_REGS-1:0][NUM_ELEM_PER_REG-1:0][NR_WRITE_PORTS-1:0] waddr_onehot_trans; // transposed index version
+  logic [NR_WRITE_PORTS-1:0][ELEM_WIDTH-1:0]                                  wdata_q;
+  logic [NR_WRITE_PORTS-1:0][NR_REGS-1:0][NUM_ELEM_PER_REG-1:0][ELEM_WBE-1:0] waddr_onehot;
+  logic [NR_REGS-1:0][NUM_ELEM_PER_REG-1:0][ELEM_WBE-1:0][NR_WRITE_PORTS-1:0] waddr_onehot_trans;
 
   for (genvar i = 0; i < NR_WRITE_PORTS; i++) begin
     for (genvar j = 0; j < NR_REGS; j++) begin
       for (genvar k = 0; k < NUM_ELEM_PER_REG; k++) begin
-        assign waddr_onehot_trans[j][k][i] = waddr_onehot[i][j][k];
+        for (genvar l = 0; l < ELEM_WBE; l++) begin
+          assign waddr_onehot_trans[j][k][l][i] = waddr_onehot[i][j][k][l];
+        end
       end
     end
   end
@@ -105,33 +111,39 @@ module vregfile #(
 
     for (genvar j = 0; j < NR_REGS; j++) begin
       for (genvar k = 0; k < NUM_ELEM_PER_REG; k++) begin
-        always_comb begin
-          if (we_i[i] && waddr_i[i][VADDR_WIDTH-1:$clog2(NUM_ELEM_PER_REG)] == j && waddr_i[i][$clog2(NUM_ELEM_PER_REG)-1:0] == k) waddr_onehot[i][j][k] = 1'b1;
-          else waddr_onehot[i][j][k] = 1'b0;
+        for (genvar l = 0; l < ELEM_WBE; l++) begin
+          always_comb begin
+            if (we_i[i] && ((wbe_i[i][l] && ENABLE_WBE) || ~ENABLE_WBE) && waddr_i[i][VADDR_WIDTH-1:$clog2(NUM_ELEM_PER_REG)] == j && waddr_i[i][$clog2(NUM_ELEM_PER_REG)-1:0] == k) waddr_onehot[i][j][k][l] = 1'b1;
+            else waddr_onehot[i][j][k][l] = 1'b0;
+          end
         end
       end
     end
   end
 
-  for (genvar i = 0; i <  NR_REGS; i++) begin
-    for (genvar j = 0; j <  NUM_ELEM_PER_REG; j++) begin
-      tc_clk_gating i_regfile_cg (
-        .clk_i     ( clk                       ),
-        .en_i      ( |waddr_onehot_trans[i][j] ),
-        .test_en_i ( 1'b0                      ),
-        .clk_o     ( mem_clocks[i][j]          )
-      );
+  for (genvar i = 0; i < NR_REGS; i++) begin
+    for (genvar j = 0; j < NUM_ELEM_PER_REG; j++) begin
+      for (genvar k = 0; k < ELEM_WBE; k++) begin
+        tc_clk_gating i_regfile_cg (
+          .clk_i     ( clk                          ),
+          .en_i      ( |waddr_onehot_trans[i][j][k] ),
+          .test_en_i ( 1'b0                         ),
+          .clk_o     ( mem_clocks[i][j][k]          )
+        );
+      end
     end
   end
 
   always_latch begin
     for (int unsigned i = 0; i < NR_REGS; i++) begin
       for (int unsigned j = 0; j < NUM_ELEM_PER_REG; j++) begin
-        for (int unsigned k = 0; k < NR_WRITE_PORTS; k++) begin
-          if (!rst_ni) begin
-            mem[i][j] = '0;
-          end if (mem_clocks[i][j]) begin
-            mem[i][j] = wdata_q[k];
+        for (int unsigned k = 0; k < ELEM_WBE; k++) begin
+          for (int unsigned l = 0; l < NR_WRITE_PORTS; l++) begin
+            if (!rst_ni) begin
+              mem[i][j] = '0;
+            end if (mem_clocks[i][j][k]) begin
+              mem[i][j][ELEM_WBE_SIZE*k +: ELEM_WBE_SIZE] = wdata_q[l][ELEM_WBE_SIZE*k +: ELEM_WBE_SIZE];
+            end
           end
         end
       end
