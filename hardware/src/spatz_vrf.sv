7a9c6a0d6ee9a2e25f57abe5befe583009d44153
Domenic Wüthrich <domenicw@student.ethz.ch>
2021-12-03 14:53:53 +0100

    [hardware] Fix vrf for synthesis and remove mem reset

--- hardware/src/spatz_vrf.sv
+++ hardware/src/spatz_vrf.sv
@@ -45,11 +45,11 @@ module spatz_vrf import spatz_pkg::*; #(
   // Functions //
   ///////////////
 
-  function vregfile_addr_t gen_vreg_addr (vreg_addr_t vaddr);
+  function automatic vregfile_addr_t gen_vreg_addr (vreg_addr_t vaddr);
     if (NrElemPerBank == 'd1) begin
       gen_vreg_addr = vaddr[$bits(vreg_addr_t)-1:$bits(vreg_addr_t)-$clog2(NRVREG)];
     end else begin
-      gen_vreg_addr = {vaddr[$bits(vreg_addr_t)-1:$bits(vreg_addr_t)-$clog2(NRVREG)], vaddr[$clog2(NrElemPerBank)-1:0]};
+      gen_vreg_addr = {vaddr[$bits(vreg_addr_t)-1:$bits(vreg_addr_t)-$clog2(NRVREG)], vaddr[cf_math_pkg::idx_width(NrElemPerBank)-1:0]};
     end
   endfunction : gen_vreg_addr
 
@@ -218,28 +218,16 @@ module vregfile #(
   output logic [NR_READ_PORTS-1:0][ELEM_WIDTH-1:0]    rdata_o
 );
 
-  localparam int unsigned NUM_ELEM_PER_REG = REG_WIDTH/ELEM_WIDTH;
-  localparam int unsigned ELEM_WBE = ENABLE_WBE ? ELEM_WIDTH/8 : 1;
-  localparam int unsigned ELEM_WBE_SIZE = ENABLE_WBE ? 8 : ELEM_WIDTH;
+  localparam int unsigned NUM_ELEM_PER_REG        = REG_WIDTH/ELEM_WIDTH;
+  localparam int unsigned NUM_BYTES_PER_ELEM      = ENABLE_WBE ? ELEM_WIDTH/8 : 1;
+  localparam int unsigned NUM_BYTES_PER_ELEM_SIZE = ENABLE_WBE ? 8 : ELEM_WIDTH;
 
   logic clk;
-  logic [NR_REGS-1:0][NUM_ELEM_PER_REG-1:0][ELEM_WBE-1:0] mem_clocks;
 
-  logic [NR_REGS-1:0][NUM_ELEM_PER_REG-1:0][ELEM_WIDTH-1:0] mem;
+  logic [NR_REGS-1:0][NUM_ELEM_PER_REG-1:0][NUM_BYTES_PER_ELEM-1:0][NUM_BYTES_PER_ELEM_SIZE-1:0] mem;
 
-  logic [NR_WRITE_PORTS-1:0][ELEM_WIDTH-1:0]                                  wdata_q;
-  logic [NR_WRITE_PORTS-1:0][NR_REGS-1:0][NUM_ELEM_PER_REG-1:0][ELEM_WBE-1:0] waddr_onehot;
-  logic [NR_REGS-1:0][NUM_ELEM_PER_REG-1:0][ELEM_WBE-1:0][NR_WRITE_PORTS-1:0] waddr_onehot_trans;
-
-  for (genvar i = 0; i < NR_WRITE_PORTS; i++) begin
-    for (genvar j = 0; j < NR_REGS; j++) begin
-      for (genvar k = 0; k < NUM_ELEM_PER_REG; k++) begin
-        for (genvar l = 0; l < ELEM_WBE; l++) begin
-          assign waddr_onehot_trans[j][k][l][i] = waddr_onehot[i][j][k][l];
-        end
-      end
-    end
-  end
+  logic [NR_WRITE_PORTS-1:0][NUM_BYTES_PER_ELEM-1:0][NUM_BYTES_PER_ELEM_SIZE-1:0]       wdata_q;
+  logic [NR_REGS-1:0][NUM_ELEM_PER_REG-1:0][NUM_BYTES_PER_ELEM-1:0][NR_WRITE_PORTS-1:0] waddr_onehot;
 
   tc_clk_gating i_regfile_cg (
     .clk_i    ( clk_i ),
@@ -248,24 +236,29 @@ module vregfile #(
     .clk_o    ( clk   )
   );
 
-  // Sample Input Data
   for (genvar i = 0; i < NR_WRITE_PORTS; i++) begin
-    always_ff @(posedge clk) wdata_q[i] <= wdata_i[i];
+    // Sample Input Data
+    always_ff @(posedge clk or negedge rst_ni) begin
+      if(~rst_ni) begin
+        wdata_q[i] <= 0;
+      end else begin
+        wdata_q[i] <= wdata_i[i];
+      end
+    end
+  end
 
+  for (genvar i = 0; i < NR_WRITE_PORTS; i++) begin
+    // Select which destination bytes to write into
     for (genvar j = 0; j < NR_REGS; j++) begin
       for (genvar k = 0; k < NUM_ELEM_PER_REG; k++) begin
-        for (genvar l = 0; l < ELEM_WBE; l++) begin
-          if (NUM_ELEM_PER_REG > 1) begin
-            always_comb begin
-              waddr_onehot[i][j][k][l] = (we_i[i] && ((wbe_i[i][l] && ENABLE_WBE) || ~ENABLE_WBE)
-                                                  && waddr_i[i][VADDR_WIDTH-1:$clog2(NUM_ELEM_PER_REG)] == j
-                                                  && waddr_i[i][$clog2(NUM_ELEM_PER_REG)-1:0] == k);
-            end
+        for (genvar l = 0; l < NUM_BYTES_PER_ELEM; l++) begin
+          if (NUM_ELEM_PER_REG == 'd1) begin
+            assign waddr_onehot[j][k][l][i] = (we_i[i] && ((wbe_i[i][l] && ENABLE_WBE) || ~ENABLE_WBE)
+                                                       && waddr_i[i][VADDR_WIDTH-1:0] == j);
           end else begin
-            always_comb begin
-              waddr_onehot[i][j][k][l] = (we_i[i] && ((wbe_i[i][l] && ENABLE_WBE) || ~ENABLE_WBE)
-                                                  && waddr_i[i][VADDR_WIDTH-1:0] == j);
-            end
+            assign waddr_onehot[j][k][l][i] = (we_i[i] && ((wbe_i[i][l] && ENABLE_WBE) || ~ENABLE_WBE)
+                                                       && waddr_i[i][VADDR_WIDTH-1:$clog2(NUM_ELEM_PER_REG)] == j
+                                                       && waddr_i[i][$clog2(NUM_ELEM_PER_REG)-1:0] == k);
           end
         end
       end
@@ -274,43 +267,33 @@ module vregfile #(
 
   for (genvar i = 0; i < NR_REGS; i++) begin
     for (genvar j = 0; j < NUM_ELEM_PER_REG; j++) begin
-      for (genvar k = 0; k < ELEM_WBE; k++) begin
-        tc_clk_gating i_regfile_cg (
-          .clk_i    ( clk                          ),
-          .en_i     ( |waddr_onehot_trans[i][j][k] ),
-          .test_en_i( 1'b0                         ),
-          .clk_o    ( mem_clocks[i][j][k]          )
-        );
-      end
-    end
-  end
+      for (genvar k = 0; k < NUM_BYTES_PER_ELEM; k++) begin
+        logic clk_latch;
 
-  always_latch begin
-    for (int unsigned i = 0; i < NR_REGS; i++) begin
-      for (int unsigned j = 0; j < NUM_ELEM_PER_REG; j++) begin
-        for (int unsigned k = 0; k < ELEM_WBE; k++) begin
-          for (int unsigned l = 0; l < NR_WRITE_PORTS; l++) begin
-            // pragma translate_off
-            if (!rst_ni) begin
-              mem[i][j] <= '0;
-            end else
-            // pragma translate_on
-            if (mem_clocks[i][j][k]) begin
-              mem[i][j][ELEM_WBE_SIZE*k +: ELEM_WBE_SIZE] <= wdata_q[l][ELEM_WBE_SIZE*k +: ELEM_WBE_SIZE];
-            end
+        tc_clk_gating i_regfile_cg (
+          .clk_i    ( clk                    ),
+          .en_i     ( |waddr_onehot[i][j][k] ),
+          .test_en_i( 1'b0                   ),
+          .clk_o    ( clk_latch              )
+        );
+
+        /* verilator lint_off NOLATCH */
+        always_latch begin
+          // TODO: Generalize for more than one write port
+          if (clk_latch) begin
+            mem[i][j][k] <= wdata_q[0][k];
           end
         end
+        /* verilator lint_on NOLATCH */
       end
     end
   end
 
-  if (NUM_ELEM_PER_REG > 1) begin
-    for (genvar i = 0; i < NR_READ_PORTS; i++) begin
+  for (genvar i = 0; i < NR_READ_PORTS; i++) begin
+    if (NUM_ELEM_PER_REG == 'd1) begin
+      assign rdata_o[i] = mem[raddr_i[i]];
+    end else begin
       assign rdata_o[i] = mem[raddr_i[i][VADDR_WIDTH-1:$clog2(NUM_ELEM_PER_REG)]][raddr_i[i][$clog2(NUM_ELEM_PER_REG)-1:0]];
-    end
-  end else begin
-    for (genvar i = 0; i < NR_READ_PORTS; i++) begin
-      assign rdata_o[i] = mem[raddr_i[i][VADDR_WIDTH-1:0]][0];
     end
   end
 
