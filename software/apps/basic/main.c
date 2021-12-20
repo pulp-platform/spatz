628da2568804c6aa9e872a84aa3521ac9483ddca
Domenic Wüthrich <domenicw@student.ethz.ch>
2021-12-20 08:13:42 +0100

    [apps] Test stripmining loop with basic program

--- software/apps/basic/main.c
+++ software/apps/basic/main.c
@@ -20,56 +20,20 @@
 
 #include <stdint.h>
 
-#define VEC_LENGTH 33
+#define UNALIGNED 1
+#define VEC_LENGTH 33+UNALIGNED
 
-uint32_t vector1[VEC_LENGTH] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 0x01};
-uint32_t vector2[VEC_LENGTH] = {0xfe, 0xdc, 0xbd, 0x98, 0x76, 0x54, 0x32, 0x10, 0xfe, 0xdc, 0xbd, 0x98, 0x76, 0x54, 0x32, 0x10, 0xfe, 0xdc, 0xbd, 0x98, 0x76, 0x54, 0x32, 0x10, 0xfe, 0xdc, 0xbd, 0x98, 0x76, 0x54, 0x32, 0x10, 0xfe};
-uint32_t vector_res[VEC_LENGTH];
+uint8_t vector1[VEC_LENGTH] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 0x01, 0x23};
+uint8_t vector2[VEC_LENGTH] = {0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10, 0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10, 0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10, 0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10, 0xfe, 0xdc};
+uint8_t vector_res[VEC_LENGTH];
 
 int main() {
-
-  uint32_t vlen = 128;
-  uint32_t actual_vlen = 0;
-  uint32_t a = 0x10;
-  uint32_t csr;
-  uint32_t val = 64;
-
-  asm volatile("vsetvli %0, %1, e8, m4, ta, ma" : "=r"(actual_vlen) : "r"(vlen));
-  asm volatile("csrrs %[csr], vl, x0" : [csr]"=r"(csr));
-  if (actual_vlen != csr) return 1;
-  asm volatile("vrsub.vx v8, v0, %[a]" :: [a]"r"(a));
-  asm volatile("vadd.vi v4, v8, 5");
-  // Shorten vl by one
-
-  // Remove
-  asm volatile("vle8.v v12, (%[vector1])" :: [vector1]"r"(vector1));
-  asm volatile("vle8.v v13, (%[vector2])" :: [vector2]"r"(vector2));
-
-  vlen -= 1;
-  asm volatile("vsetvli %0, %1, e8, m4, ta, ma" : "=r"(actual_vlen) : "r"(vlen));
-  asm volatile("csrrs %[csr], vl, x0" : [csr]"=r"(csr));
-
-  // Remove
-  asm volatile("vse8.v v12, (%[vector_res])" :: [vector_res]"r"(vector_res));
-  asm volatile("vse8.v v13, (%[vector_res])" :: [vector_res]"r"(vector_res+1));
-
-  if (actual_vlen != csr) return 2;
-  // Set vstart to one (second element)
-  asm volatile("csrrwi x0, vstart, 1");
-  // Clear registers and execute macc
-  asm volatile("vadd.vi v0, v0, 2");
-  asm volatile("vadd.vx v12, v12, %[val]" :: [val]"r"(val));
-  asm volatile("vadd.vi v28, v28, 5");
-  asm volatile("vmacc.vv v28, v0, v12");
-  asm volatile("vadd.vi v20, v28, 0");
-  // Execute instruction with vs1 and vs2 as source, and vd as destination
-  asm volatile("vsub.vv v16, v20, v0");
   // Stripmining loop
-  /*uint32_t remaining_elem = VEC_LENGTH;
-  uint32_t *ptr_vec1 = vector1;
-  uint32_t *ptr_vec2 = vector2;
-  uint32_t *ptr_vec_res = vector_res;
-  while (remaining_elem != 0) {
+  int32_t remaining_elem = VEC_LENGTH-UNALIGNED;
+  uint8_t *ptr_vec1 = vector1+UNALIGNED;
+  uint8_t *ptr_vec2 = vector2+UNALIGNED;
+  uint8_t *ptr_vec_res = vector_res+UNALIGNED;
+  while (remaining_elem > 0) {
     uint32_t actual_elem;
     asm volatile("vsetvli %0, %1, e8, m1, ta, ma" : "=r"(actual_elem) : "r"(remaining_elem));
     asm volatile("vle8.v v12, (%[vector1])" :: [vector1]"r"(ptr_vec1));
@@ -81,16 +45,9 @@ int main() {
     ptr_vec2 += actual_elem;
     ptr_vec_res += actual_elem;
   }
-  for (int i = 0; i < VEC_LENGTH; i++) {
-    if (vector1[i] + vector2[i] != vector_res[i]) return 1;
-  }*/
-
-  // Remove
-  if (vector1[0] != vector_res[0]) return 2;
-  if (vector2[0] != vector_res[1]) return 3;
-
-  // Buffer instruction to make sure last instruction will finish within simulation
-  asm volatile("csrrs %[csr], vl, x0" : [csr]"=r"(csr));
+  for (int i = UNALIGNED; i < VEC_LENGTH; i++) {
+    if (vector1[i] + vector2[i] != vector_res[i]) return i+1;
+  }
 
   return 0;
 }
