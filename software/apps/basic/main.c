7a42b7c3f4e7e2359f2836659498c851407c89c3
Domenic Wüthrich <domenicw@student.ethz.ch>
2022-02-03 16:50:44 +0100

    [apps] Extend basic app with slide instruction

--- software/apps/basic/main.c
+++ software/apps/basic/main.c
@@ -27,6 +27,9 @@ uint8_t vector1[VEC_LENGTH] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 0
 uint8_t vector2[VEC_LENGTH] = {0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10, 0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10, 0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10, 0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10, 0xfe, 0xdc};
 uint8_t vector_res[VEC_LENGTH];
 
+uint8_t vector_sld[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+uint8_t vector_sld_res[16];
+
 int main() {
   // Stripmining loop
   int32_t remaining_elem = VEC_LENGTH-UNALIGNED;
@@ -48,6 +51,29 @@ int main() {
   for (int i = UNALIGNED; i < VEC_LENGTH; i++) {
     if (vector1[i] + vector2[i] != vector_res[i]) return i+1;
   }
+
+  // Vector slide1up
+  uint8_t elem_insert = 0xff;
+  asm volatile("vsetivli zero, 16, e8, m1, ta, ma");
+  asm volatile("vle8.v v8, (%0)" :: "r"(vector_sld));
+  asm volatile("vslide1up.vx v9, v8, %0" :: "r"(elem_insert));
+  asm volatile("vse8.v v9, (%0)" :: "r"(vector_sld_res));
+
+  if (vector_sld_res[0] != elem_insert) return 1;
+  for (int i = 1; i < 16; i++) {
+    if (vector_sld_res[i] != vector_sld[i-1]) return i+1;
+  }
+
+  // Vector slide1down
+  asm volatile("vsetivli zero, 16, e8, m1, ta, ma");
+  asm volatile("vle8.v v6, (%0)" :: "r"(vector_sld));
+  asm volatile("vslide1down.vx v7, v6, %0" :: "r"(elem_insert));
+  asm volatile("vse8.v v7, (%0)" :: "r"(vector_sld_res));
+
+  for (int i = 0; i < 15; i++) {
+    if (vector_sld_res[i] != vector_sld[i-1]) return i+1;
+  }
+  if (vector_sld_res[15] != elem_insert) return 16;
 
   return 0;
 }
