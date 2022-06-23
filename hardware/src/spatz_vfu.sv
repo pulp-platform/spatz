472712f3f4aba1498a645eb39e89321904056e7f
Matheus Cavalcante <matheusd@iis.ee.ethz.ch>
2022-06-23 18:51:47 +0200

    [hardware] Rewrite the VFU

--- hardware/src/spatz_vfu.sv
+++ hardware/src/spatz_vfu.sv
@@ -33,99 +33,122 @@ module spatz_vfu import spatz_pkg::*; import rvv_pkg::*; import cf_math_pkg::idx
 // Include FF
 `include "common_cells/registers.svh"
 
-  /////////////
-  // Signals //
-  /////////////
+  ///////////////////////
+  //  Operation queue  //
+  ///////////////////////
 
-  // Spatz request
-  spatz_req_t spatz_req_d, spatz_req_q;
-  `FF(spatz_req_q, spatz_req_d, '0)
+  spatz_req_t spatz_req;
+  logic       spatz_req_valid;
+  logic       spatz_req_ready;
+
+  spill_register #(
+    .T(spatz_req_t)
+  ) i_operation_queue (
+    .clk_i  (clk_i                                          ),
+    .rst_ni (rst_ni                                         ),
+    .data_i (spatz_req_i                                    ),
+    .valid_i(spatz_req_valid_i && spatz_req_i.ex_unit == VFU),
+    .ready_o(spatz_req_ready_o                              ),
+    .data_o (spatz_req                                      ),
+    .valid_o(spatz_req_valid                                ),
+    .ready_i(spatz_req_ready                                )
+  );
+
+  ///////////////
+  //  Control  //
+  ///////////////
+
+  // Vector length counter
+  vlen_t vl_q, vl_d;
+  `FF(vl_q, vl_d, '0)
+
+  // Are we busy?
+  logic busy_q, busy_d;
+  `FF(busy_q, busy_d, 1'b0)
+
+  // Number of words advanced by vstart
+  vlen_t vstart;
+  assign vstart = ((spatz_req.vstart / N_IPU) >> (EW_32 - spatz_req.vtype.vsew)) << (EW_32 - spatz_req.vtype.vsew);
 
   // Are the VFU operands ready?
   logic op1_is_ready, op2_is_ready, op3_is_ready, operands_ready;
-  assign op1_is_ready   = !spatz_req_q.use_vs1 || vrf_rvalid_i[1];
-  assign op2_is_ready   = !spatz_req_q.use_vs2 || vrf_rvalid_i[0];
-  assign op3_is_ready   = !spatz_req_q.vd_is_src || vrf_rvalid_i[2];
+  assign op1_is_ready   = spatz_req_valid && (!spatz_req.use_vs1 || vrf_rvalid_i[1]);
+  assign op2_is_ready   = spatz_req_valid && (!spatz_req.use_vs2 || vrf_rvalid_i[0]);
+  assign op3_is_ready   = spatz_req_valid && (!spatz_req.vd_is_src || vrf_rvalid_i[2]);
   assign operands_ready = op1_is_ready && op2_is_ready && op3_is_ready;
 
   // Did we write a result back to the VRF?
   logic result_written;
-  assign result_written = !spatz_req_q.use_vd || vrf_wvalid_i;
+  assign result_written = spatz_req_valid && (!spatz_req.use_vd || vrf_wvalid_i);
 
-  // Is vl and vstart zero?
-  logic vl_is_zero;
-  logic vstart_is_zero;
-  assign vl_is_zero     = spatz_req_q.vl == 0;
-  assign vstart_is_zero = spatz_req_q.vstart == 0;
-
-  // Number of elements in one VRF word
-  logic [$clog2(N_IPU*4):0] nr_elem_word;
-  assign nr_elem_word = N_IPU * (1 << (EW_32 - spatz_req_q.vtype.vsew));
-
-  // Did we reach the last elements of the instruction?
-  logic last_word;
-  assign last_word = spatz_req_q.vl <= vlen_t'(nr_elem_word);
   // Did we commit a word to the VRF?
   logic word_committed;
   assign word_committed = operands_ready && result_written;
 
-  // Is the VFU ready for a new request from the controller?
-  logic vfu_is_ready;
-  assign vfu_is_ready      = vl_is_zero || (last_word && word_committed);
-  assign spatz_req_ready_o = vfu_is_ready;
+  // Number of elements in one VRF word
+  logic [$clog2(N_IPU*4):0] nr_elem_word;
+  assign nr_elem_word = N_IPU * (1 << (EW_32 - spatz_req.vtype.vsew));
 
-  // Did a new VFU execution request arrive?
-  logic new_request;
-  assign new_request = spatz_req_valid_i && vfu_is_ready && (spatz_req_i.ex_unit == VFU);
+  // Did we reach the last elements of the instruction?
+  logic last_word;
+  assign last_word = spatz_req.vl <= vlen_t'(nr_elem_word);
+
+  always_comb begin: control_proc
+    // Maintain state
+    vl_d   = vl_q;
+    busy_d = busy_q;
+
+    // We are handling an instruction
+    spatz_req_ready = 1'b0;
+
+    // Do not ack anything
+    vfu_rsp_valid_o = 1'b0;
+    vfu_rsp_o       = '0;
+
+    // Change number of remaining elements
+    if (word_committed)
+      vl_d = vl_q + nr_elem_word;
+
+    // Finished the execution!
+    if (spatz_req_valid && vl_d >= spatz_req.vl) begin
+      spatz_req_ready = spatz_req_valid;
+      busy_d          = 1'b0;
+
+      vfu_rsp_o.id    = spatz_req.id;
+      vfu_rsp_o.vs2   = spatz_req.vs2;
+      vfu_rsp_o.vs1   = spatz_req.vs1;
+      vfu_rsp_o.vd    = spatz_req.vd;
+      vfu_rsp_valid_o = 1'b1;
+    end
+    // Do we have a new instruction?
+    else if (spatz_req_valid && !busy_d) begin
+      // Start at vstart
+      vl_d   = vstart;
+      busy_d = 1'b1;
+    end
+  end: control_proc
+
+  //////////////
+  // Operands //
+  //////////////
 
   // IPU operands and result signals
   logic [N_IPU*ELEN-1:0] operand1, operand2, operand3;
   logic [N_IPU*ELEN-1:0] result;
-  assign operand1 = spatz_req_q.use_vs1 ? vrf_rdata_i[1] :
-                    spatz_req_q.vtype.vsew == EW_8 ? {4*N_IPU{spatz_req_q.rs1[7:0]}}   :
-                    spatz_req_q.vtype.vsew == EW_16 ? {2*N_IPU{spatz_req_q.rs1[15:0]}} :
-                                                      {1*N_IPU{spatz_req_q.rs1}};
-  assign operand2 = vrf_rdata_i[0];
-  assign operand3 = vrf_rdata_i[2];
-
-  // Number of words advanced by vstart
-  vlen_t vstart_nwords;
-  assign vstart_nwords = vlen_t'(((spatz_req_i.vstart / N_IPU) >> (EW_32 - spatz_req_i.vtype.vsew)) << (EW_32 - spatz_req_i.vtype.vsew));
-
-  ///////////////////
-  // State Handler //
-  ///////////////////
-
-  always_comb begin : proc_state_handler
-    // Maintain state
-    spatz_req_d = spatz_req_q;
-
-    if (new_request) begin
-      spatz_req_d    = spatz_req_i;
-      // Decrement vl by vstart
-      spatz_req_d.vl = spatz_req_i.vl - vstart_nwords;
-    end else if (!vl_is_zero && word_committed) begin
-      // Change number of remaining elements
-      spatz_req_d.vl     = last_word ? '0 : spatz_req_q.vl - nr_elem_word;
-      spatz_req_d.vstart = '0;
-    end else if (vl_is_zero) begin
-      spatz_req_d = '0;
+  always_comb begin: operand_proc
+    if (spatz_req.use_vs1)
+      operand1 = vrf_rdata_i[1];
+    else begin
+      // Replicate scalar operands
+      unique case (spatz_req.vtype.vsew)
+        EW_8 : operand1 = {4*N_IPU{spatz_req.rs1[7:0]}};
+        EW_16: operand1 = {2*N_IPU{spatz_req.rs1[15:0]}};
+        EW_32: operand1 = {1*N_IPU{spatz_req.rs1}};
+      endcase
     end
-  end : proc_state_handler
-
-  // Respond to controller when we are finished executing
-  always_comb begin : proc_vfu_rsp
-    vfu_rsp_valid_o = 1'b0;
-    vfu_rsp_o       = '0;
-
-    if (last_word && word_committed) begin
-      vfu_rsp_o.id    = spatz_req_q.id;
-      vfu_rsp_o.vs2   = spatz_req_q.vs2;
-      vfu_rsp_o.vs1   = spatz_req_q.vs1;
-      vfu_rsp_o.vd    = spatz_req_q.vd;
-      vfu_rsp_valid_o = 1'b1;
-    end
-  end
+    operand2 = vrf_rdata_i[0];
+    operand3 = vrf_rdata_i[2];
+  end: operand_proc
 
   ///////////////////////
   // Operand Requester //
@@ -140,50 +163,54 @@ module spatz_vfu import spatz_pkg::*; import rvv_pkg::*; import cf_math_pkg::idx
   `FF(vreg_addr_q, vreg_addr_d, '0)
 
   // Calculate new vector register address
-  always_comb begin : proc_vreg_addr
+  always_comb begin : vreg_addr_proc
     vreg_addr_d = vreg_addr_q;
 
-    if (new_request) begin
-      vreg_addr_d[0] = (spatz_req_i.vs2 + vstart_nwords) << $clog2(NrWordsPerVector);
-      vreg_addr_d[1] = (spatz_req_i.vs1 + vstart_nwords) << $clog2(NrWordsPerVector);
-      vreg_addr_d[2] = (spatz_req_i.vd + vstart_nwords) << $clog2(NrWordsPerVector);
-    end else if (!vl_is_zero && !last_word && word_committed) begin
+    if (spatz_req_valid && !busy_q) begin
+      vreg_addr_d[0] = (spatz_req_i.vs2 + vstart) << $clog2(NrWordsPerVector);
+      vreg_addr_d[1] = (spatz_req_i.vs1 + vstart) << $clog2(NrWordsPerVector);
+      vreg_addr_d[2] = (spatz_req_i.vd + vstart) << $clog2(NrWordsPerVector);
+    end else if (spatz_req_valid && vl_q < spatz_req.vl && word_committed) begin
       vreg_addr_d[0] = vreg_addr_q[0] + 1;
       vreg_addr_d[1] = vreg_addr_q[1] + 1;
       vreg_addr_d[2] = vreg_addr_q[2] + 1;
-    end else if (vl_is_zero || (last_word && word_committed)) begin
+    end else if (vl_q >= spatz_req.vl || (last_word && word_committed)) begin
       vreg_addr_d = '0;
     end
-  end
+  end: vreg_addr_proc
 
-  always_comb begin : proc_op_req
-    automatic vlen_t vend       = spatz_req_q.vtype.vsew == EW_8 ? spatz_req_q.vl[idx_width(N_IPU * 4):0]       : spatz_req_q.vtype.vsew == EW_16 ? spatz_req_q.vl[idx_width(N_IPU * 2):0]       : spatz_req_q.vl[idx_width(N_IPU):0];
-    automatic vlen_t vstart     = spatz_req_q.vtype.vsew == EW_8 ? spatz_req_q.vstart[idx_width(N_IPU * 4)-1:0] : spatz_req_q.vtype.vsew == EW_16 ? spatz_req_q.vstart[idx_width(N_IPU * 2)-1:0] : spatz_req_q.vstart[idx_width(N_IPU)-1:0];
-    automatic vlen_t subtrahend = !vstart_is_zero ? vstart                                                      : vend;
-    automatic vlen_t shift      = nr_elem_word - subtrahend;
-
+  always_comb begin : operand_req_proc
     vreg_r_req = '0;
     vreg_we    = '0;
     vreg_wbe   = '0;
 
-    if (!vl_is_zero) begin
+    if (vl_q < spatz_req.vl) begin
       // Request operands
-      vreg_r_req = {spatz_req_q.vd_is_src, spatz_req_q.use_vs1, spatz_req_q.use_vs2};
+      vreg_r_req = {spatz_req.vd_is_src, spatz_req.use_vs1, spatz_req.use_vs2};
 
       // Distribute operands and write result back to register file
       if (operands_ready) begin
-        vreg_we  = spatz_req_q.use_vd;
+        vreg_we  = spatz_req.use_vd;
         vreg_wbe = '1;
 
         // If we are in the last group or at the start and vstart is nonzero,
         // create the byte enable (be) mask for write back to register file.
-        if (last_word || !vstart_is_zero) begin
-          vreg_wbe = vreg_be_t'('1) >> shift;
-          vreg_wbe = !vstart_is_zero ? ~vreg_wbe : vreg_wbe;
-        end
+        if (spatz_req.vstart != '0 && busy_q == 1'b0)
+          unique case (spatz_req.vtype.vsew)
+            EW_8 : vreg_wbe = ~(vreg_be_t'('1) >> (nr_elem_word - spatz_req.vstart[idx_width(N_IPU * 4)-1:0]));
+            EW_16: vreg_wbe = ~(vreg_be_t'('1) >> (nr_elem_word - spatz_req.vstart[idx_width(N_IPU * 2)-1:0]));
+            EW_32: vreg_wbe = ~(vreg_be_t'('1) >> (nr_elem_word - spatz_req.vstart[idx_width(N_IPU * 1)-1:0]));
+          endcase
+        else
+          if (last_word)
+            unique case (spatz_req.vtype.vsew)
+              EW_8 : vreg_wbe = vreg_be_t'('1) >> vl_q[idx_width(N_IPU * 4):0];
+              EW_16: vreg_wbe = vreg_be_t'('1) >> vl_q[idx_width(N_IPU * 2):0];
+              EW_32: vreg_wbe = vreg_be_t'('1) >> vl_q[idx_width(N_IPU * 1):0];
+            endcase
       end
     end
-  end : proc_op_req
+  end : operand_req_proc
 
   // Register file signals
   assign vrf_raddr_o = vreg_addr_q;
@@ -201,13 +228,13 @@ module spatz_vfu import spatz_pkg::*; import rvv_pkg::*; import cf_math_pkg::idx
     spatz_ipu i_ipu (
       .clk_i            (clk_i                     ),
       .rst_ni           (rst_ni                    ),
-      .operation_i      (spatz_req_q.op            ),
+      .operation_i      (spatz_req.op              ),
       .operation_valid_i(!vl_is_zero               ), // If the VFU is not ready, it is executing something
       .op_s1_i          (operand1[ipu*ELEN +: ELEN]),
       .op_s2_i          (operand2[ipu*ELEN +: ELEN]),
       .op_d_i           (operand3[ipu*ELEN +: ELEN]),
       .carry_i          ('0                        ),
-      .sew_i            (spatz_req_q.vtype.vsew    ),
+      .sew_i            (spatz_req.vtype.vsew      ),
       .be_o             (/* Unused */              ),
       .result_o         (result[ipu*ELEN +: ELEN]  )
     );
