a53a7541076fb6395b19d90ab14253e9aa76b486
Matheus Cavalcante <matheusd@iis.ee.ethz.ch>
2023-03-11 17:43:35 +0100

    [software] Write an axpy kernel

--- software/apps/axpy/main.c
+++ software/apps/axpy/main.c
@@ -16,109 +16,171 @@
 
 // Author: Domenic Wüthrich
 
+#include <stdbool.h>
 #include <stdint.h>
-#include <stdio.h>
 #include <string.h>
 
+#include "data/data_axpy.h"
 #include "printf.h"
 #ifdef MEMPOOL
+#include "alloc.h"
 #include "runtime.h"
 #include "synchronization.h"
 #endif
 
-#ifndef N_IPU
-#define N_IPU 2
-#endif
-#ifndef MATRIX_DIM
-#define MATRIX_DIM 64
-#endif
-#ifndef KERNEL_M
-#define KERNEL_M 1
-#endif
-#ifndef KERNEL_P
-#define KERNEL_P 8
-#endif
+double *x;
+double *y;
 
-#define SIZE 8*KERNEL_P
+// Initialize the matrices
+void init_matrix(double *matrix, const double *src,
+                 const unsigned int num_columns) {
+  for (unsigned int j = 0; j < num_columns; ++j)
+    matrix[j] = src[j];
+}
 
-int32_t vec_l1[SIZE*NUM_CORES] __attribute__((aligned(4), section(".l1_prio")));
-
-int verify_vector(int32_t *vector, int32_t size, int32_t alpha) {
-  for (int idx = 0; idx < size; idx++) {
-    if (vector[idx] != idx*alpha + idx) {
-      return idx == 0 ? -1 : idx;
-    }
+// Verify the matrices
+int verify_matrix(double *matrix, const double *golden,
+                  const unsigned int num_columns) {
+  for (unsigned int j = 0; j < num_columns; ++j) {
+    double diff = matrix[j] - golden[j];
+    if (diff < 0)
+      diff = -diff;
+    if (diff > 0.001)
+      return j == 0 ? -1 : (int)j;
   }
   return 0;
 }
 
-void init_vector(int32_t *vector, int32_t size) {
-  for (int idx = 0; idx < size; idx++) {
-    vector[idx] = idx;
-  }
-}
-
 int main() {
-  uint32_t num_cores = mempool_get_core_count();
-  uint32_t core_id = mempool_get_core_id();
-  int32_t vec_stack[SIZE];
-  int32_t *vector;
+  const unsigned int num_cores = mempool_get_core_count();
+  const unsigned int cores_per_group = num_cores / NUM_GROUPS;
+  const unsigned int cid = mempool_get_core_id();
 
-  int32_t alpha = 2;
+  const unsigned int active_groups = 1;
+  const unsigned int active_cores = cores_per_group * active_groups;
+  const unsigned int is_core_active = cid < active_cores;
 
-  #if KERNEL_M == 1
-    vector = vec_stack;
-  #else
-    vector = vec_l1+SIZE*core_id;
-  #endif
-  init_vector(vector, SIZE);
+  unsigned int timer_start, timer_end, timer;
 
-  #ifdef DISABLE_MULTICORE
-    num_cores = 1;
-  #else
-    mempool_barrier_init(core_id);
-    mempool_barrier(num_cores);
-  #endif
+  unsigned int vl;
+  unsigned int dim;
 
-  asm volatile("vsetvli zero, %0, e32, m8, ta, ma" :: "r"(SIZE));
+  // Initialize MemPool
+  mempool_init(cid, num_cores);
 
-  uint32_t timer_start = mempool_get_timer();
+  // Initialize multicore barrier
+  mempool_barrier_init(cid);
 
-  asm volatile("vle32.v v0, (%0)" :: "r"(vector));
-  asm volatile("vle32.v v8, (%0)" :: "r"(vector));
-  asm volatile("vmacc.vx v0, %0, v8" :: "r"(alpha));
-  asm volatile("vse32.v v0, (%0)" :: "r"(vector));
-
-  #ifdef DISABLE_MULTICORE
-    asm volatile("lw zero, 0(%0)" :: "r"(vector));
-  #else
-    mempool_barrier(num_cores);
-  #endif
-
-  // Performance metrics
-  uint32_t timer_end = mempool_get_timer();
-
-  if (core_id == 0) {
-    uint32_t runtime = timer_end - timer_start - 1;
-    uint32_t performance = 1000 * 2 * SIZE*num_cores / runtime;
-    uint32_t perm_of_max = performance * 6 / (N_IPU*4 * num_cores);
-
-    printf("The execution took %u cycles.\n", runtime);
-    printf("The performance is %u OP/1000cycles (%u%%o permille of max).\n",
-           performance, perm_of_max);
-    // Verify correctness
-    printf("Verifying result...\n");
+  // Allocate the matrices in the local tile
+  if (cid == 0) {
+    x = (double *)domain_malloc(get_alloc_tile(0), axpy_l.M * sizeof(double));
+    y = (double *)domain_malloc(get_alloc_tile(0), axpy_l.M * sizeof(double));
   }
 
-  int error = verify_vector(vector, SIZE, alpha);
-  if (error != 0) {
-    printf("Fail.\n");
-    return (int)core_id*SIZE+error;
+  // Reset timer
+  timer = (unsigned int)-1;
+
+  // Set matrix dimension
+  dim = axpy_l.M;
+  vl = axpy_l.M / active_cores;
+
+  // Wait for all cores to finish
+  mempool_barrier(num_cores);
+
+  // Initialize matrices
+  if (cid == 0) {
+    init_matrix(x, axpy_X_dram, dim);
+    init_matrix(y, axpy_Y_dram, dim);
   }
 
-  #ifndef DISABLE_MULTICORE
-    mempool_barrier(num_cores);
-  #endif
+  // Initialize alpha
+  double alpha = axpy_alpha_dram;
+
+  // Calculate local pointers
+  double *x_ = x + cid * vl;
+  double *y_ = y + cid * vl;
+
+  // Wait for all cores to finish
+  mempool_barrier(num_cores);
+
+  // Calculate AXPY
+  if (is_core_active) {
+    // Start timer
+    timer_start = mempool_get_timer();
+
+    // Start dump
+    if (cid == 0)
+      start_kernel();
+
+    // Stripmining loop
+    while (vl > 0) {
+      // Set the VL
+      size_t gvl;
+      asm volatile("vsetvli %[gvl], %[vl], e64, m8, ta, ma"
+                   : [gvl] "=r"(gvl)
+                   : [vl] "r"(vl));
+
+      // Load vectors
+      asm volatile("vle64.v v0, (%0)" ::"r"(x_));
+      asm volatile("vle64.v v16, (%0)" ::"r"(y_));
+
+      // Multiply-accumulate
+      asm volatile("vfmacc.vf v16, %0, v0" ::"f"(alpha));
+
+      // Store results
+      asm volatile("vse64.v v16, (%0)" ::"r"(y_));
+
+      // Bump pointers
+      x_ += gvl;
+      y_ += gvl;
+      vl -= gvl;
+    }
+  }
+
+  // Wait for all cores to finish matmul
+  mempool_barrier(num_cores);
+
+  // End dump
+  if (cid == 0)
+    stop_kernel();
+
+  // End timer and check if new best runtime
+  timer_end = mempool_get_timer();
+  if (cid == 0) {
+    unsigned int timer_temp = timer_end - timer_start;
+    if (timer_temp < timer) {
+      timer = timer_temp;
+    }
+  }
+
+  // Check and display results
+  if (cid == 0) {
+    unsigned int performance = 1000 * 2 * dim / timer;
+    unsigned int utilization = performance / (2 * active_cores * N_FPU);
+
+    printf("\n----- (%d) axpy -----\n", dim);
+    printf("The execution took %u cycles.\n", timer);
+    printf("The performance is %u OP/1000cycle (%u%%o utilization).\n",
+           performance, utilization);
+  }
+
+  if (cid == 0) {
+    int error = verify_matrix(y, (const double *)axpy_GR_dram, dim);
+
+    if (error != 0) {
+      printf("Error: y[%d]=%04x\n", error, *(uint32_t *)(y + error));
+      return error;
+    }
+  }
+
+  // Free the matrices
+  if (cid == 0) {
+    domain_free(get_alloc_tile(0), x);
+    domain_free(get_alloc_tile(0), y);
+  }
+
+  // Wait for core 0 to finish displaying results
+  mempool_barrier(num_cores);
 
   return 0;
 }
