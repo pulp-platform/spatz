9942801bbf176c79f9a4c1f5dbb60cb817211c56
Matheus Cavalcante <matheusd@iis.ee.ethz.ch>
2023-03-27 16:14:49 +0200

    [hardware] :fire: Remove unused scripts

--- hardware/scripts/wave.tcl
+++
@@ -1,8 +0,0 @@
-# Copyright 2021 ETH Zurich and University of Bologna.
-# Licensed under the Apache License, Version 2.0, see LICENSE for details.
-# SPDX-License-Identifier: Apache-2.0
-
-# Author: Domenic Wüthrich, ETH Zurich
-
-add wave -Group CLK /spatz_tb/dut/clk_i
-add wave -Group CLK /spatz_tb/dut/rst_ni
