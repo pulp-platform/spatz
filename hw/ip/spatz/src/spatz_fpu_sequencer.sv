b0265762c1ff098c50885c8133ec3072fecd4713
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2023-08-15 21:32:20 +0200

    hw/ip/spatz: add mxu mem ops to load/store list

--- hw/ip/spatz/src/spatz_fpu_sequencer.sv
+++ hw/ip/spatz/src/spatz_fpu_sequencer.sv
@@ -549,7 +549,10 @@ module spatz_fpu_sequencer
     {riscv_instr::VLE8_V, riscv_instr::VLE16_V, riscv_instr::VLE32_V, riscv_instr::VLE64_V,
     riscv_instr::VLSE8_V, riscv_instr::VLSE16_V, riscv_instr::VLSE32_V, riscv_instr::VLSE64_V,
     riscv_instr::VLOXEI8_V, riscv_instr::VLOXEI16_V, riscv_instr::VLOXEI32_V, riscv_instr::VLOXEI64_V,
-    riscv_instr::VLUXEI8_V, riscv_instr::VLUXEI16_V, riscv_instr::VLUXEI32_V, riscv_instr::VLUXEI64_V};
+    riscv_instr::VLUXEI8_V, riscv_instr::VLUXEI16_V, riscv_instr::VLUXEI32_V, riscv_instr::VLUXEI64_V,
+    riscv_instr::MLE8_V_A, riscv_instr::MLE8_V_B, riscv_instr::MLE8_V_C, riscv_instr::MLE16_V_A,
+    riscv_instr::MLE16_V_B, riscv_instr::MLE16_V_C, riscv_instr::MLE32_V_A, riscv_instr::MLE32_V_B,
+    riscv_instr::MLE32_V_C};
 
   // Is the current instruction a vector store?
   logic is_vector_store;
@@ -557,7 +560,10 @@ module spatz_fpu_sequencer
     {riscv_instr::VSE8_V, riscv_instr::VSE16_V, riscv_instr::VSE32_V, riscv_instr::VSE64_V,
     riscv_instr::VSSE8_V, riscv_instr::VSSE16_V, riscv_instr::VSSE32_V, riscv_instr::VSSE64_V,
     riscv_instr::VSOXEI8_V, riscv_instr::VSOXEI16_V, riscv_instr::VSOXEI32_V, riscv_instr::VSOXEI64_V,
-    riscv_instr::VSUXEI8_V, riscv_instr::VSUXEI16_V, riscv_instr::VSUXEI32_V, riscv_instr::VSUXEI64_V};
+    riscv_instr::VSUXEI8_V, riscv_instr::VSUXEI16_V, riscv_instr::VSUXEI32_V, riscv_instr::VSUXEI64_V,
+    riscv_instr::MSE8_V_A, riscv_instr::MSE8_V_B, riscv_instr::MSE8_V_C, riscv_instr::MSE16_V_A,
+    riscv_instr::MSE16_V_B, riscv_instr::MSE16_V_C, riscv_instr::MSE32_V_A, riscv_instr::MSE32_V_B,
+    riscv_instr::MSE32_V_C};
 
   // Do we need to delay is load/store because of the VLSU?
   assign vlsu_stall = (is_store && acc_mem_cnt_q != '0) || (is_load && acc_mem_str_cnt_q != '0) || acc_mem_cnt_q == '1;
