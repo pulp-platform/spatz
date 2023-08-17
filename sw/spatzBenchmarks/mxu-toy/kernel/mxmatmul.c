e6989ecc98c0d459ab887a8abd42584e5d3d67c0
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2023-08-17 19:39:39 +0200

    sw/spatzBenchmarks: parametrize mxy-toy program

--- sw/spatzBenchmarks/mxu-toy/kernel/mxmatmul.c
+++ sw/spatzBenchmarks/mxu-toy/kernel/mxmatmul.c
@@ -22,12 +22,17 @@
 #endif
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
+
+#define TILE_LD_A
+#define TILE_LD_B
+#define MXMACC_TEST
+
 void matmul_tiled_load_store_test(uint32_t *c, const uint32_t *a, const uint32_t *b,
                  const unsigned int dim) {
-  
+
   /* Vector Length Setting*/
   asm volatile("vsetvli zero, %0, e32, m4, ta, ma" ::"r"(16));
-  
+
   /* Tile Load Settings*/
   //asm volatile("msettilem t1, %0" ::"r" (8));
   //asm volatile("msettilen t2, %0" ::"r" (8));
@@ -35,7 +40,7 @@ void matmul_tiled_load_store_test(uint32_t *c, const uint32_t *a, const uint32_t
 
   /* Clean up result register*/
   asm volatile("vmv.v.i v0, 0");
-  
+
   /* Input and Output address*/
   const uint32_t *a_ = a;
   const uint32_t *c_ = c;
@@ -52,33 +57,37 @@ void matmul_tiled_load_store_test(uint32_t *c, const uint32_t *a, const uint32_t
 
 void matmul_tiled_mxmacc_test(uint32_t *c, const uint32_t *a, const uint32_t *b,
                  const unsigned int dim) {
-  
+
   /* Vector Length Setting*/
   asm volatile("vsetvli zero, %0, e32, m4, ta, ma" ::"r"(32));
   const uint32_t *a_ = a;
   const uint32_t *b_ = b;
   const uint32_t *c_ = c;
-  
+
 
   /* Tile Load A*/
+#ifdef TILE_LD_A
   asm volatile("msettilem t1, %0" ::"r" (8));
   asm volatile("msettilen t2, %0" ::"r" (4));
-  //asm volatile("msettilek t3, %0" ::"r" (8)); // k never work for anything
   asm volatile("vmv.v.i v0, 0");
   asm volatile("mle32.v.a v0, (%0), %1;" ::"r"(a_), "r"(4));
   //asm volatile("vse32.v v0, (%0);" ::"r"(c_)); // Optional Print
+#endif
 
   /* Tile Load B*/
+#ifdef TILE_LD_B
   asm volatile("msettilem t1, %0" ::"r" (8));
   asm volatile("msettilen t2, %0" ::"r" (4));
-  //asm volatile("msettilek t3, %0" ::"r" (8)); // k never work for anything
   asm volatile("vmv.v.i v4, 0");
   asm volatile("mle32.v.b v4, (%0), %1;" ::"r"(b_), "r"(4));
   asm volatile("vse32.v v4, (%0);" ::"r"(c_)); // Optional Print
+#endif
 
   /* mxmacc Testing*/
+#ifdef MXMACC_TEST
   asm volatile("vmv.v.i v8, 0");
   asm volatile("mxmacc.vv v8, v0, v4");
   //asm volatile("mse32.v.c v8, (%0), %1;" ::"r"(c_), "r"(8));
   //asm volatile("vse32.v v8, (%0);" ::"r"(c_)); // Optional Print
+#endif
 }
