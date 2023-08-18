a6c456e2d86018f35c5fc878fa8c1f19234e4506
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2023-08-18 22:20:20 +0200

    sw/spatzBenchmarks: convert mxu-toy to 64-bit

--- sw/spatzBenchmarks/mxu-toy/kernel/mxmatmul.c
+++ sw/spatzBenchmarks/mxu-toy/kernel/mxmatmul.c
@@ -34,9 +34,9 @@ void matmul_tiled_load_store_test(double *c, const double *a, const double *b,
   asm volatile("vsetvli zero, %0, e64, m4, ta, ma" ::"r"(16));
 
   /* Tile Load Settings*/
-  //asm volatile("msettilem t1, %0" ::"r" (8));
-  //asm volatile("msettilen t2, %0" ::"r" (8));
-  //asm volatile("msettilek t3, %0" ::"r" (8));
+  //asm volatile("msettilem t1, %0" ::"r" (8):"t1");
+  //asm volatile("msettilen t2, %0" ::"r" (8):"t2");
+  //asm volatile("msettilek t3, %0" ::"r" (8):"t3");
 
   /* Clean up result register*/
   asm volatile("vmv.v.i v0, 0");
@@ -47,12 +47,12 @@ void matmul_tiled_load_store_test(double *c, const double *a, const double *b,
 
   /* Load and Store Testing*/
   // Load test (Tile load + Vector store)
-  //asm volatile("mle32.v.a v0, (%0), %1;" ::"r"(a_), "r"(dim));
-  //asm volatile("vse32.v v0, (%0);" ::"r"(c_));
+  //asm volatile("mle64.v.a v0, (%0), %1;" ::"r"(a_), "r"(dim));
+  //asm volatile("vse64.v v0, (%0);" ::"r"(c_));
 
   // Store test (Vector load + Tile store)
-  asm volatile("vle32.v v0, (%0);" ::"r"(a_));
-  asm volatile("mse32.v.c v0, (%0), %1;" ::"r"(c_), "r"(8));
+  asm volatile("vle64.v v0, (%0);" ::"r"(a_));
+  asm volatile("mse64.v.c v0, (%0), %1;" ::"r"(c_), "r"(8));
 }
 
 void matmul_tiled_mxmacc_test(double *c, const double *a, const double *b,
@@ -67,27 +67,27 @@ void matmul_tiled_mxmacc_test(double *c, const double *a, const double *b,
 
   /* Tile Load A*/
 #ifdef TILE_LD_A
-  asm volatile("msettilem t1, %0" ::"r" (8));
-  asm volatile("msettilen t2, %0" ::"r" (4));
+  asm volatile("msettilem t1, %0" ::"r" (8):"t1");
+  asm volatile("msettilen t2, %0" ::"r" (4):"t2");
   asm volatile("vmv.v.i v0, 0");
-  asm volatile("mle32.v.a v0, (%0), %1;" ::"r"(a_), "r"(4));
-  asm volatile("vse32.v v0, (%0);" ::"r"(c_)); // Optional Print
+  asm volatile("mle64.v.a v0, (%0), %1;" ::"r"(a_), "r"(4));
+  asm volatile("vse64.v v0, (%0);" ::"r"(c_)); // Optional Print
 #endif
 
   /* Tile Load B*/
 #ifdef TILE_LD_B
-  asm volatile("msettilem t1, %0" ::"r" (8));
-  asm volatile("msettilen t2, %0" ::"r" (4));
+  asm volatile("msettilem t1, %0" ::"r" (8):"t1");
+  asm volatile("msettilen t2, %0" ::"r" (4):"t2");
   asm volatile("vmv.v.i v4, 0");
-  asm volatile("mle32.v.b v4, (%0), %1;" ::"r"(b_), "r"(4));
-  asm volatile("vse32.v v4, (%0);" ::"r"(c_)); // Optional Print
+  asm volatile("mle64.v.b v4, (%0), %1;" ::"r"(b_), "r"(4));
+  asm volatile("vse64.v v4, (%0);" ::"r"(c_)); // Optional Print
 #endif
 
   /* mxmacc Testing*/
 #ifdef MXMACC_TEST
   asm volatile("vmv.v.i v8, 0");
   asm volatile("mxmacc.vv v8, v0, v4");
-  //asm volatile("mse32.v.c v8, (%0), %1;" ::"r"(c_), "r"(8));
-  //asm volatile("vse32.v v8, (%0);" ::"r"(c_)); // Optional Print
+  //asm volatile("mse64.v.c v8, (%0), %1;" ::"r"(c_), "r"(8));
+  //asm volatile("vse64.v v8, (%0);" ::"r"(c_)); // Optional Print
 #endif
 }
