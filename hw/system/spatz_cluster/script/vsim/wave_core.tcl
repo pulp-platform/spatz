3a645287a122aa9daf4aa3083468e3d31eec8aab
Navaneeth-Kunhi Purayil <nkunhi@iis.ee.ethz.ch>
2024-11-09 14:18:04 +0100

    [hw/ip/spatz] added logic to buffer FPU data if conflict with VLSU ports When there is a conflict between FPU and VLSU INT0 or INT1, then VLSU interfaces are preferred and while the FPU data is buffered into a spill register. From FPU perspective it is as if the data has been written. But from a scoreboard perspective, the FPU write is still pending.

--- hw/system/spatz_cluster/script/vsim/wave_core.tcl
+++ hw/system/spatz_cluster/script/vsim/wave_core.tcl
@@ -129,6 +129,8 @@ add wave -noupdate -group core[$1] -group Spatz /tb_bin/i_dut/i_cluster_wrapper/
 add wave -noupdate -group core[$1] -group Spatz /tb_bin/i_dut/i_cluster_wrapper/i_cluster/gen_core[$1]/i_spatz_cc/i_spatz/spatz_mem_rsp_i
 add wave -noupdate -group core[$1] -group Spatz /tb_bin/i_dut/i_cluster_wrapper/i_cluster/gen_core[$1]/i_spatz_cc/i_spatz/spatz_mem_rsp_valid_i
 
+add wave -noupdate -group core[$1] -group Spatz -group Buffering /tb_bin/i_dut/i_cluster_wrapper/i_cluster/gen_core[$1]/i_spatz_cc/i_spatz/i_vfu_buf/*
+
 add wave -noupdate -group core[$1] -group Spatz -group "FPU Sequencer" /tb_bin/i_dut/i_cluster_wrapper/i_cluster/gen_core[$1]/i_spatz_cc/i_spatz/gen_fpu_sequencer/i_fpu_sequencer/*
 add wave -noupdate -group core[$1] -group Spatz -group "FPU Sequencer" -group FPR /tb_bin/i_dut/i_cluster_wrapper/i_cluster/gen_core[$1]/i_spatz_cc/i_spatz/gen_fpu_sequencer/i_fpu_sequencer/i_fpr/*
 add wave -noupdate -group core[$1] -group Spatz -group "FPU Sequencer" -group LSU /tb_bin/i_dut/i_cluster_wrapper/i_cluster/gen_core[$1]/i_spatz_cc/i_spatz/gen_fpu_sequencer/i_fpu_sequencer/i_fp_lsu/*
