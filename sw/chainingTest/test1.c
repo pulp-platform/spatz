6f668d4e2959bc0630cb997e21245596302be607
Navaneeth-Kunhi Purayil <nkunhi@iis.ee.ethz.ch>
2024-11-15 09:41:08 +0100

    [sw] added chaining test files

---
+++ sw/chainingTest/test1.c
@@ -0,0 +1,32 @@
+// Copyright 2021 ETH Zurich and University of Bologna.
+// Solderpad Hardware License, Version 0.51, see LICENSE for details.
+// SPDX-License-Identifier: SHL-0.51
+//
+// Author: Navaneeth Kunhi Purayil <nkunhi@iis.ee.ethz.ch>
+//
+
+#include "vector_macros.h"
+
+void TEST32(void) {
+  VSET(32, e64, m8);
+  VLOAD_64(v0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32);
+  asm volatile("vadd.vi v8, v0, 1");
+  VCMP_U64(1, v8, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33);
+}
+
+void TEST64(void) {
+  VSET(64, e64, m8);
+  VLOAD_64(v0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32);
+  asm volatile("vadd.vi v8, v0, 1");
+  VCMP_U64(2, v8, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33);
+}
+
+int main(void) {
+  INIT_CHECK();
+  enable_vec();
+
+  TEST32();
+  TEST64();
+
+  EXIT_CHECK();
+}
