db2f8544cb90fc0a39d61a7132f9d01b1e22264d
Navaneeth-Kunhi Purayil <nkunhi@iis.ee.ethz.ch>
2025-01-17 18:49:20 +0100

    [spatz] added a fifo between VLSU1 and VRF to buffer data during conflicts The axpy kernel does not complete here, but we see performance improvements for dotp dotp_4096 : 72.2 % fmatmul_64x64x64 : 97.8 %

--- hw/ip/spatz/src/spatz.sv
+++ hw/ip/spatz/src/spatz.sv
@@ -77,6 +77,7 @@ module spatz import spatz_pkg::*; import rvv_pkg::*; import fpnew_pkg::*; #(
   
   // FPU buffer size (need atleast depth of 2 to hide conflicts)
   localparam int unsigned FpuBufDepth = 4;
+  localparam int unsigned VlsuBufDepth = 2;
 
   /////////////
   // Signals //
@@ -92,19 +93,20 @@ module spatz import spatz_pkg::*; import rvv_pkg::*; import fpnew_pkg::*; #(
   vfu_rsp_t vfu_rsp, vfu_rsp_buf;
 
   logic      vlsu_req_ready;
-  logic      vlsu_rsp_valid;
-  vlsu_rsp_t vlsu_rsp;
+  logic      vlsu_rsp_valid, vlsu_rsp_buf_valid;
+  vlsu_rsp_t vlsu_rsp, vlsu_rsp_buf;
 
   logic       vsldu_req_ready;
   logic       vsldu_rsp_valid;
   vsldu_rsp_t vsldu_rsp;
 
   // Signals for buffering of FPU
-  logic vrf_buf_en;
-  logic vrf_vfu_wvalid;
+  logic vrf_buf_en, vlsu_buf_en;
+  logic vrf_vfu_wvalid, vrf_vlsu_wvalid;
 
-  logic buf_full, buf_empty;
-  logic [$clog2(FpuBufDepth)-1:0] buf_usage;
+  logic buf_full, buf_empty, vlsu_buf_full, vlsu_buf_empty;
+  logic [$clog2(FpuBufDepth)-1:0] buf_usage; 
+  logic [$clog2(VlsuBufDepth)-1:0] vlsu_buf_usage;
   
   // Buffer structure to track data information for writes from FPU to VRF
   typedef struct packed {
@@ -116,7 +118,17 @@ module spatz import spatz_pkg::*; import rvv_pkg::*; import fpnew_pkg::*; #(
     logic rsp_valid;
   } vrf_buf_t;
 
-  vrf_buf_t vrf_buf_data;
+  typedef struct packed {
+    vrf_data_t wdata;
+    vrf_addr_t waddr;
+    vrf_be_t   wbe;
+    spatz_id_t wid;
+    vlsu_rsp_t rsp;
+    logic rsp_valid;
+  } vlsu_buf_t;
+
+  vrf_buf_t vrf_buf_data; 
+  vlsu_buf_t vlsu_buf_data;
 
   /////////////////////
   //  FPU sequencer  //
@@ -220,7 +232,8 @@ module spatz import spatz_pkg::*; import rvv_pkg::*; import fpnew_pkg::*; #(
 
   spatz_vrf #(
     .NrReadPorts (NrReadPorts ),
-    .NrWritePorts(NrWritePorts)
+    .NrWritePorts(NrWritePorts),
+    .FpuBufDepth(FpuBufDepth)
   ) i_vrf (
     .clk_i     (clk_i     ),
     .rst_ni    (rst_ni    ),
@@ -280,8 +293,8 @@ module spatz import spatz_pkg::*; import rvv_pkg::*; import fpnew_pkg::*; #(
     .vfu_rsp_i        (vfu_rsp_buf     ),
     // VLSU
     .vlsu_req_ready_i (vlsu_req_ready  ),
-    .vlsu_rsp_valid_i (vlsu_rsp_valid  ),
-    .vlsu_rsp_i       (vlsu_rsp        ),
+    .vlsu_rsp_valid_i (vlsu_rsp_buf_valid  ),
+    .vlsu_rsp_i       (vlsu_rsp_buf    ),
     // VLSD
     .vsldu_req_ready_i(vsldu_req_ready ),
     .vsldu_rsp_valid_i(vsldu_rsp_valid ),
@@ -355,6 +368,30 @@ module spatz import spatz_pkg::*; import rvv_pkg::*; import fpnew_pkg::*; #(
   );
   assign vrf_vfu_wvalid = sb_we[VFU_VD_WD] && !buf_full;
 
+  // Buffering of VLSU1 when conflicting with VLSU0
+  assign vlsu_buf_en =  sb_we[VLSU_VD_WD1] && (!vrf_wvalid[VLSU_VD_WD1] || (vrf_wvalid[VLSU_VD_WD1] && !vlsu_buf_empty));
+  fifo_v3 #(
+    .FALL_THROUGH (1'b1         ),
+    .dtype        (vlsu_buf_t   ),
+    .DEPTH        (VlsuBufDepth )
+  ) i_vlsu_buf (
+    .clk_i      (clk_i),
+    .rst_ni     (rst_ni),
+    .flush_i    (1'b0), 
+    .testmode_i (1'b0),
+
+    .full_o     (vlsu_buf_full),
+    .empty_o    (vlsu_buf_empty),
+    .usage_o    (vlsu_buf_usage),
+
+    .data_i     ({vrf_wdata[VLSU_VD_WD1], vrf_waddr[VLSU_VD_WD1], vrf_wbe[VLSU_VD_WD1], sb_id[SB_VLSU_VD_WD1], vlsu_rsp, vlsu_rsp_valid}), 
+    .push_i     (vlsu_buf_en && !vlsu_buf_full),
+
+    .data_o     (vlsu_buf_data),
+    .pop_i      (vrf_wvalid[VLSU_VD_WD1] && !vlsu_buf_empty)
+  );
+  assign vrf_vlsu_wvalid = sb_we[VLSU_VD_WD1] && !vlsu_buf_full;
+
 `endif
 
   always_comb begin
@@ -364,8 +401,11 @@ module spatz import spatz_pkg::*; import rvv_pkg::*; import fpnew_pkg::*; #(
     vrf_waddr_buf = vrf_waddr;
     vrf_wbe_buf = vrf_wbe;
     sb_buf_id = sb_id;
+    // Responses
     vfu_rsp_buf = vfu_rsp;
     vfu_rsp_buf_valid = vfu_rsp_valid;
+    vlsu_rsp_buf = vlsu_rsp;
+    vlsu_rsp_buf_valid = vlsu_rsp_valid;
     // If buffer is used and has valid data, use the buffered data
 `ifdef BUF_FPU
     if (!buf_empty) begin
@@ -380,6 +420,22 @@ module spatz import spatz_pkg::*; import rvv_pkg::*; import fpnew_pkg::*; #(
       // If the buffer is being enabled in this cycle, don't send the response now
       if (vrf_buf_en) begin
         vfu_rsp_buf_valid = 1'b0;
+      end
+    end
+
+    // VLSU1 buffering
+    if (!vlsu_buf_empty) begin
+      sb_we_buf    [VLSU_VD_WD1] = 1'b1;
+      vrf_wdata_buf[VLSU_VD_WD1] = vlsu_buf_data.wdata;
+      vrf_waddr_buf[VLSU_VD_WD1] = vlsu_buf_data.waddr;
+      vrf_wbe_buf  [VLSU_VD_WD1] = vlsu_buf_data.wbe;
+      sb_buf_id    [SB_VLSU_VD_WD1] = vlsu_buf_data.wid;
+      vlsu_rsp_buf = vlsu_buf_data.rsp;
+      vlsu_rsp_buf_valid = vlsu_buf_data.rsp_valid & vrf_wvalid[VLSU_VD_WD1];
+    end else begin
+      // If the buffer is being enabled in this cycle, don't send the response now
+      if (vlsu_buf_en) begin
+        vlsu_rsp_buf_valid = 1'b0;
       end
     end
 `endif
@@ -408,7 +464,11 @@ module spatz import spatz_pkg::*; import rvv_pkg::*; import fpnew_pkg::*; #(
     .vrf_wdata_o             (vrf_wdata[VLSU_VD_WD1:VLSU_VD_WD0]                   ),
     .vrf_we_o                (sb_we[VLSU_VD_WD1:VLSU_VD_WD0]                       ),
     .vrf_wbe_o               (vrf_wbe[VLSU_VD_WD1:VLSU_VD_WD0]                     ),
+`ifdef BUF_FPU
+    .vrf_wvalid_i            ({vrf_vlsu_wvalid, vrf_wvalid[VLSU_VD_WD0]}           ),
+`else
     .vrf_wvalid_i            (vrf_wvalid[VLSU_VD_WD1:VLSU_VD_WD0]                  ),
+`endif
     .vrf_raddr_o             (vrf_raddr[VLSU_VD_RD1:VLSU_VS2_RD0]                  ),
     .vrf_re_o                (sb_re[VLSU_VD_RD1:VLSU_VS2_RD0]                      ),
     .vrf_rdata_i             (vrf_rdata[VLSU_VD_RD1:VLSU_VS2_RD0]                  ),
