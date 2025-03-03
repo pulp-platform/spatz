58a48239d917d07c21b7b5e0d1e7fae395e22438
Max Wipfli <mwipfli@ethz.ch>
2025-03-03 14:36:40 +0100

    riscvTests: Fix and enable vsetvli test

--- sw/riscvTests/isa/rv64uv/vsetvli.c
+++ sw/riscvTests/isa/rv64uv/vsetvli.c
@@ -4,26 +4,51 @@
 //
 // Author: Matheus Cavalcante <matheusd@iis.ee.ethz.ch>
 //         Basile Bougenot <bbougenot@student.ethz.ch>
+//         Max Wipfli <mwipfli@ethz.ch>
 
+#include "encoding.h"
 #include "vector_macros.h"
 #include <stdint.h>
+
+void TEST_CASE1() {
+  uint32_t vl, avl;
+  uint32_t vlenb = read_csr(vlenb);
+
+  avl = 314;
+  __asm__ volatile("vsetvli %[vl], %[avl], e8, m2, ta, ma" : [vl]"=r"(vl) : [avl] "r"(avl));
+  XCMP(1, vl, snrt_min(avl, vlenb * 2));
+
+  avl = 15;
+  __asm__ volatile("vsetvli %[vl], %[avl], e16, m2, ta, ma" : [vl]"=r"(vl) : [avl] "r"(avl));
+  XCMP(2, vl, snrt_min(avl, vlenb * 2 / 2));
+
+  avl = 255;
+  __asm__ volatile("vsetvli %[vl], %[avl], e32, m2, ta, ma" : [vl]"=r"(vl) : [avl] "r"(avl));
+  XCMP(3, vl, snrt_min(avl, vlenb * 2 / 4));
+
+  avl = 69;
+  __asm__ volatile("vsetvli %[vl], %[avl], e64, m8, ta, ma" : [vl]"=r"(vl) : [avl] "r"(avl));
+  XCMP(4, vl, snrt_min(avl, vlenb * 8 / 8));
+
+  // SEW=128 not supported
+  // avl = 69;
+  // __asm__ volatile("vsetvli %[vl], %[avl], e128, m8, ta, ma" : [vl]"=r"(vl) : [avl] "r"(avl));
+  // XCMP(5, vl, snrt_min(avl, vlenb * 2 / 16));
+
+  avl = 15;
+  __asm__ volatile("vsetvli %[vl], %[avl], e8, m8, ta, ma" : [vl]"=r"(vl) : [avl] "r"(avl));
+  XCMP(6, vl, snrt_min(avl, vlenb * 8));
+
+  avl = 10000;
+  __asm__ volatile("vsetvli %[vl], %[avl], e16, m2, ta, ma" : [vl]"=r"(vl) : [avl] "r"(avl));
+  XCMP(7, vl, snrt_min(avl, vlenb * 2 / 2));
+}
 
 int main(void) {
   INIT_CHECK();
   enable_vec();
-  uint64_t scalar = 314;
-  __asm__ volatile("vsetvli t0, %[A], e8, m2" ::[A] "r"(scalar));
-  scalar = 15;
-  __asm__ volatile("vsetvli t0, %[A], e16, m2" ::[A] "r"(scalar));
-  scalar = 255;
-  __asm__ volatile("vsetvli t0, %[A], e32, m2" ::[A] "r"(scalar));
-  scalar = 69;
-  __asm__ volatile("vsetvli t0, %[A], e64, m8" ::[A] "r"(scalar));
-  scalar = 69;
-  __asm__ volatile("vsetvli t0, %[A], e128, m8" ::[A] "r"(scalar));
-  scalar = 15;
-  __asm__ volatile("vsetvli t0, %[A], e8, m8" ::[A] "r"(scalar));
-  scalar = 10000;
-  __asm__ volatile("vsetvli t0, %[A], e16, m2" ::[A] "r"(scalar));
-  return (0);
+
+  TEST_CASE1();
+
+  EXIT_CHECK();
 }
