0fc7cab88a963dcfb1427383af58e68dd3989d1d
Danilo Cammarata <dcammarata@iis.ee.ethz.ch>
2025-04-01 17:09:06 +0200

    Fixed Quadrilatero toolchain bugs

--- util/iis-env.sh
+++ util/iis-env.sh
@@ -8,5 +8,3 @@
 echo "export Spatz toolchains"
 export LLVM_INSTALL_DIR=/usr/pack/riscv-1.0-kgf/spatz-llvm-2023.08.10
 export GCC_INSTALL_DIR=/usr/pack/riscv-1.0-kgf/spatz-gcc-7.1.1
-export LLVM_INSTALL_DIR_QUADRILATERO=/usr/scratch/larain10/dcammarata/spatz/sw/toolchain/quadrilatero-tc/temp/install
-export GCC_INSTALL_DIR_QUADRILATERO=/usr/scratch/larain10/dcammarata/spatz/sw/toolchain/quadrilatero-tc/temp/install
