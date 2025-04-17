a15ee721fdc91be3c51666daa8d640e850baf41c
Max Wipfli <mail@maxwipfli.ch>
2025-04-17 12:11:39 +0200

    spatz_controller: Update scoreboard for vmxdotp.wf

--- hw/ip/spatz/src/spatz_controller.sv
+++ hw/ip/spatz/src/spatz_controller.sv
@@ -442,6 +442,10 @@ module spatz_controller
     // Initialize the scoreboard metadata if we have a new instruction issued.
     if (spatz_req_valid && spatz_req.ex_unit != CON) begin
       // RAW hazard
+      if (spatz_req.use_vs4) begin
+        scoreboard_d[spatz_req.id].deps[write_table_d[spatz_req.vs4].id] |= write_table_d[spatz_req.vs4].valid;
+        read_table_d[spatz_req.vs4] = {spatz_req.id, 1'b1};
+      end
       if (spatz_req.use_vs2) begin
         scoreboard_d[spatz_req.id].deps[write_table_d[spatz_req.vs2].id] |= write_table_d[spatz_req.vs2].valid;
         read_table_d[spatz_req.vs2] = {spatz_req.id, 1'b1};
