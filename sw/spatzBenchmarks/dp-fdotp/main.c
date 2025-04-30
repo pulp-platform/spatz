af833af95c1f70a01a0a8be45c77e163328e1708
Hong Pang <hopang@larain5.ee.ethz.ch>
2025-04-30 20:34:40 +0200

    [SW] Remove UART initialization part to Flamingo SoC tb level

--- sw/spatzBenchmarks/dp-fdotp/main.c
+++ sw/spatzBenchmarks/dp-fdotp/main.c
@@ -18,7 +18,6 @@
 
 #include <benchmark.h>
 #include <snrt.h>
-#include <uart.h>
 #include <stdio.h>
 
 #include DATAHEADER
@@ -40,8 +39,6 @@ static inline int fp_check(const double a, const double b) {
 }
 
 int main() {
-  void *base = (void *)__base_uart;
-  uart_init(base, 800000000, 115200);
 
   const unsigned int num_cores = snrt_cluster_core_num();
   const unsigned int cid = snrt_cluster_core_idx();
