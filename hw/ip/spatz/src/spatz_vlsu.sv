af7b27c790552c7dd19aca689468212b57d9c224
rgiunti <riccardo.giunti@chips.it>
2025-11-05 15:50:18 +0100

    [FIX/HW] fix indexed addr gen

--- hw/ip/spatz/src/spatz_vlsu.sv
+++ hw/ip/spatz/src/spatz_vlsu.sv
@@ -434,16 +434,23 @@ module spatz_vlsu
     maxew_t idx_offset;
     assign idx_offset = mem_idx_counter_q[port];
 
+    logic signed [2:0] data_index_width_diff;
+    logic [idx_width(N_FU*ELENB)-1:0] word_index;
+
     always_comb begin
       stride = mem_is_strided ? mem_spatz_req.rs2 >> mem_spatz_req.vtype.vsew : 'd1;
 
       if (mem_is_indexed) begin
         // What is the relationship between data and index width?
-        automatic logic [1:0] data_index_width_diff = int'(mem_spatz_req.vtype.vsew) - int'(mem_spatz_req.op_mem.ew);
+        data_index_width_diff = int'(mem_spatz_req.vtype.vsew) - int'(mem_spatz_req.op_mem.ew);
 
         // Pointer to index
-        automatic logic [idx_width(N_FU*ELENB)-1:0] word_index = (port << (MAXEW - data_index_width_diff)) + (maxew_t'(idx_offset << data_index_width_diff) >> data_index_width_diff) + (maxew_t'(idx_offset >> (MAXEW - data_index_width_diff)) << (MAXEW - data_index_width_diff)) * NrMemPorts;
-
+        if (data_index_width_diff >= 0) begin
+          word_index = (port << (MAXEW - data_index_width_diff)) + (maxew_t'(idx_offset << data_index_width_diff) >> data_index_width_diff) + (maxew_t'(idx_offset >> (MAXEW - data_index_width_diff)) << (MAXEW - data_index_width_diff)) * NrMemPorts;
+        end else begin
+          word_index = (port >> (MAXEW + data_index_width_diff)) + (maxew_t'(idx_offset >> -data_index_width_diff) << -data_index_width_diff) + (maxew_t'(idx_offset << (MAXEW + data_index_width_diff)) >> (MAXEW + data_index_width_diff)) * NrMemPorts;
+        end
+        
         // Index
         unique case (mem_spatz_req.op_mem.ew)
           EW_8 : offset   = $signed(vrf_rdata_i[1][8 * word_index +: 8]);
