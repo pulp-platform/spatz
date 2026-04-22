358650fe42738bec0e38bdbd34d26c6d68041adc
Danilo Cammarata <dcammarata@iis.ee.ethz.ch>
2026-04-22 12:37:05 +0200

    [LINT] Modified quadrilatero.sv.tpl to fix lint errors on comments

--- hw/system/spatz_cluster/src/generated/quadrilatero_pkg.sv
+++ hw/system/spatz_cluster/src/generated/quadrilatero_pkg.sv
@@ -1,8 +1,8 @@
-// Copyright 2024 EPFL
-// Solderpad Hardware License, Version 2.1, see LICENSE.md for details.
-// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+// Copyright 2026 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
 //
-// Author: Danilo Cammarata
+// Author: Danilo Cammarata, ETH Zurich
 
 package quadrilatero_pkg;
 
