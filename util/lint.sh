378b8d8b80f7e7157f002df88ea9abc5062457d6
bowwang <bowwang@iis.ee.ethz.ch>
2026-05-27 12:54:44 +0200

    [ci] add python deps

--- util/lint.sh
+++ util/lint.sh
@@ -29,12 +29,7 @@ CF=install/llvm/bin/clang-format; [ ! -d install/llvm ] && CF="$LLVM_INSTALL_DIR
 
 # Check python files
 echo "Check Python files"
-if [[ -n "$GITLAB_CI" ]]; then
-  # Special case for gitlab ci, since it has an old default python
-  python3=$(command -v python3.6) || EXIT_STATUS=$?
-else
-  python3=$(command -v python3) || EXIT_STATUS=$?
-fi
+python3=$(command -v python3) || EXIT_STATUS=$?
 ${python3} -m flake8 --ignore=E501,W503,E203 sw/snRuntime || EXIT_STATUS=$?
 ${python3} -m flake8 --ignore=E501,W503,E203 sw/spatzBenchmarks || EXIT_STATUS=$?
 ${python3} -m flake8 --ignore=E501,W503,E203 util || EXIT_STATUS=$?
