4f78bb4bc50daa9234c7b133ff04fda568b916d5
rgiunti <riccardo.giunti@chips.it>
2026-07-22 16:33:16 +0200

    [HW] VLSEG/VSSEG decoding

--- hw/ip/spatz/src/spatz_controller.sv
+++ hw/ip/spatz/src/spatz_controller.sv
@@ -639,7 +639,7 @@ module spatz_controller
       end
 
       // Is this a risky instruction which should not chain?
-      if (spatz_req.op inside {VSLIDEUP, VLSE, VLXE, VSSE, VSXE})
+      if (spatz_req.op inside {VSLIDEUP, VLSE, VLSEG, VLXE, VSSE, VSSEG, VSXE})
         scoreboard_d[spatz_req.id].prevent_chaining = 1'b1;
 
       // Is this a narrowing or widening instruction?
