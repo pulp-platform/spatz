417d28066c7420de1c65dd4c81452c0336a5e927
bowwang <bowwang@iis.ee.ethz.ch>
2026-07-27 16:14:33 +0200

    [tests] fix inline-asm memory clobbers and type errors

--- sw/riscvTests/isa/rv64uv/vloxei.c
+++ sw/riscvTests/isa/rv64uv/vloxei.c
@@ -14,7 +14,7 @@ void TEST_CASE1(void) {
 
   VSET(2, e8, m1);
   VLOAD_8(v2, 1, 15);
-  asm volatile("vloxei8.v v1, (%0), v2" ::"r"(&ALIGNED_I8[0]));
+  asm volatile("vloxei8.v v1, (%0), v2" ::"r"(&ALIGNED_I8[0]) : "memory");
   VCMP_U8(1, v1, 0xd3, 0x89);
 
   volatile uint16_t ALIGNED_I16[] = {
@@ -23,7 +23,7 @@ void TEST_CASE1(void) {
 
   VSET(2, e16, m1);
   VLOAD_16(v2, 2, 30);
-  asm volatile("vloxei16.v v1, (%0), v2" ::"r"(&ALIGNED_I16[0]));
+  asm volatile("vloxei16.v v1, (%0), v2" ::"r"(&ALIGNED_I16[0]) : "memory");
   VCMP_U16(2, v1, 0xbbd3, 0x1989);
 
   volatile uint32_t ALIGNED_I32[] = {
@@ -33,7 +33,7 @@ void TEST_CASE1(void) {
 
   VSET(2, e32, m1);
   VLOAD_32(v2, 4, 60);
-  asm volatile("vloxei32.v v1, (%0), v2" ::"r"(&ALIGNED_I32[0]));
+  asm volatile("vloxei32.v v1, (%0), v2" ::"r"(&ALIGNED_I32[0]) : "memory");
   VCMP_U32(3, v1, 0xf9aa71f0, 0x89139848);
 }
 
@@ -46,7 +46,7 @@ void TEST_CASE2(void) {
 
   VSET(2, e16, m1);
   VLOAD_8(v2, 2, 30);
-  asm volatile("vloxei8.v v1, (%0), v2" ::"r"(&ALIGNED_I16[0]));
+  asm volatile("vloxei8.v v1, (%0), v2" ::"r"(&ALIGNED_I16[0]) : "memory");
   VCMP_U16(4, v1, 0xbbd3, 0x1989);
 
   volatile uint32_t ALIGNED_I32[] = {
@@ -56,7 +56,7 @@ void TEST_CASE2(void) {
 
   VSET(2, e32, m1);
   VLOAD_16(v2, 4, 60);
-  asm volatile("vloxei16.v v1, (%0), v2" ::"r"(&ALIGNED_I32[0]));
+  asm volatile("vloxei16.v v1, (%0), v2" ::"r"(&ALIGNED_I32[0]) : "memory");
   VCMP_U32(5, v1, 0xf9aa71f0, 0x89139848);
 
 #if ELEN == 64
@@ -70,7 +70,7 @@ void TEST_CASE2(void) {
 
   VSET(2, e64, m1);
   VLOAD_32(v2, 8, 120);
-  asm volatile("vloxei32.v v1, (%0), v2" ::"r"(&ALIGNED_I64[0]));
+  asm volatile("vloxei32.v v1, (%0), v2" ::"r"(&ALIGNED_I64[0]) : "memory");
   VCMP_U64(6, v1, 0xf9aa71f0c394bbd3, 0x8913984898951989);
 #endif
 }
@@ -82,7 +82,7 @@ void TEST_CASE3(void) {
 
   VSET(2, e8, m1);
   VLOAD_16(v2, 1, 15);
-  asm volatile("vloxei16.v v1, (%0), v2" ::"r"(&ALIGNED_I8[0]));
+  asm volatile("vloxei16.v v1, (%0), v2" ::"r"(&ALIGNED_I8[0]) : "memory");
   VCMP_U8(7, v1, 0xd3, 0x89);
 
   volatile uint16_t ALIGNED_I16[] = {
@@ -91,7 +91,7 @@ void TEST_CASE3(void) {
 
   VSET(2, e16, m1);
   VLOAD_32(v2, 2, 30);
-  asm volatile("vloxei32.v v1, (%0), v2" ::"r"(&ALIGNED_I16[0]));
+  asm volatile("vloxei32.v v1, (%0), v2" ::"r"(&ALIGNED_I16[0]) : "memory");
   VCMP_U16(8, v1, 0xbbd3, 0x1989);
 
 }
@@ -110,7 +110,7 @@ void TEST_CASE4(void) {
   VCLEAR(v2);
   VLOAD_8(v1, 99, 99);
   VLOAD_8(v2, 1, 15);
-  asm volatile("vloxei8.v v1, (%0), v2, v0.t" ::"r"(&ALIGNED_I8[0]));
+  asm volatile("vloxei8.v v1, (%0), v2, v0.t" ::"r"(&ALIGNED_I8[0]) : "memory");
   VCMP_U8(9, v1, 99, 0x89);
 
   volatile uint16_t ALIGNED_I16[] = {
@@ -123,7 +123,7 @@ void TEST_CASE4(void) {
   VLOAD_16(v1, 999, 999);
   VLOAD_16(v2, 2, 30);
 
-  asm volatile("vloxei16.v v1, (%0), v2, v0.t" ::"r"(&ALIGNED_I16[0]));
+  asm volatile("vloxei16.v v1, (%0), v2, v0.t" ::"r"(&ALIGNED_I16[0]) : "memory");
   VCMP_U16(10, v1, 999, 0x1989);
 
   volatile uint32_t ALIGNED_I32[] = {
@@ -136,7 +136,7 @@ void TEST_CASE4(void) {
   VCLEAR(v2);
   VLOAD_32(v1, 999, 999);
   VLOAD_32(v2, 4, 60);
-  asm volatile("vloxei32.v v1, (%0), v2, v0.t" ::"r"(&ALIGNED_I32[0]));
+  asm volatile("vloxei32.v v1, (%0), v2, v0.t" ::"r"(&ALIGNED_I32[0]) : "memory");
   VCMP_U32(11, v1, 999, 0x89139848);
 
 /*#if ELEN == 64
@@ -152,7 +152,7 @@ void TEST_CASE4(void) {
   VLOAD_64(v1, 999, 999);
   VLOAD_64(v2, 8, 120);
   VLOAD_8(v0, 0xAA);
-  asm volatile("vloxei64.v v1, (%0), v2, v0.t" ::"r"(&ALIGNED_I64[0]));
+  asm volatile("vloxei64.v v1, (%0), v2, v0.t" ::"r"(&ALIGNED_I64[0]) : "memory");
   VCMP_U64(12, v1, 999, 0x8913984898951989);
 #endif*/
 }
@@ -164,7 +164,7 @@ void TEST_CASE5(void) {
   VSET(12, e8, m1);
 
   VLOAD_8(v2, 1, 2, 3, 4, 5, 7, 8, 9, 11, 12, 13, 15);
-  asm volatile("vloxei8.v v1, (%0), v2" ::"r"(&ALIGNED_I8[0]));
+  asm volatile("vloxei8.v v1, (%0), v2" ::"r"(&ALIGNED_I8[0]) : "memory");
   VCMP_U8(13, v1, 0xd3, 0x40, 0xd1, 0x84, 0x48, 0x88, 0x88, 0xae, 0x91, 0x02,
           0x59, 0x89);
 
@@ -175,7 +175,7 @@ void TEST_CASE5(void) {
 
   VSET(12, e16, m1);
   VLOAD_16(v2, 2, 4, 6, 8, 10, 14, 16, 18, 22, 24, 26, 30);
-  asm volatile("vloxei16.v v1, (%0), v2" ::"r"(&ALIGNED_I16[0]));
+  asm volatile("vloxei16.v v1, (%0), v2" ::"r"(&ALIGNED_I16[0]) : "memory");
   VCMP_U16(14, v1, 0xbbd3, 0x3840, 0x8cd1, 0x9384, 0x7548, 0x9388, 0x8188,
            0x11ae, 0x4891, 0x4902, 0x8759, 0x1989);
 
@@ -187,14 +187,14 @@ void TEST_CASE5(void) {
 
   /*VSET(12, e32, m1);
   VLOAD_32(v2, 4, 8, 12, 16, 20, 28, 32, 36, 44, 48, 52, 60);
-  asm volatile("vloxei32.v v1, (%0), v2" ::"r"(&ALIGNED_I32[0]));
+  asm volatile("vloxei32.v v1, (%0), v2" ::"r"(&ALIGNED_I32[0]) : "memory");
   VCMP_U32(15, v1, 0xf9aa71f0, 0xa11a9384, 0x99991348, 0x9fa831c7, 0x38197598,
            0x81937598, 0x18747547, 0x3eeeeeee, 0xab8b9148, 0x90318509,
            0x31897598, 0x89139848);*/
 
   VSET(8, e32, m1);
     VLOAD_32(v2, 4, 8, 12, 16, 20, 28, 32, 36);
-    asm volatile("vloxei32.v v1, (%0), v2" ::"r"(&ALIGNED_I32[0]));
+    asm volatile("vloxei32.v v1, (%0), v2" ::"r"(&ALIGNED_I32[0]) : "memory");
     VCMP_U32(16, v1, 0xf9aa71f0, 0xa11a9384, 0x99991348, 0x9fa831c7, 0x38197598,
             0x81937598, 0x18747547, 0x3eeeeeee);
 }
@@ -210,7 +210,7 @@ void TEST_CASE6(void) {
   VLOAD_8(v2, 1, 15, 1, 3, 10);
   // Modify vstart val
   asm volatile("csrs vstart, %0" ::"r"(1));
-  asm volatile("vloxei8.v v1, (%0), v2" ::"r"(&ALIGNED_I8[0]));
+  asm volatile("vloxei8.v v1, (%0), v2" ::"r"(&ALIGNED_I8[0]) : "memory");
   VCMP_U8(17, v1, 0, 0x89, 0xd3, 0xd1, 0x08);
 
 
@@ -223,7 +223,7 @@ void TEST_CASE6(void) {
   VSET(16, e16, m1);
   VLOAD_16(v2, 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30);
   asm volatile("csrs vstart, %0" ::"r"(5));
-  asm volatile("vloxei16.v v1, (%0), v2" ::"r"(&ALIGNED_I16[0]));
+  asm volatile("vloxei16.v v1, (%0), v2" ::"r"(&ALIGNED_I16[0]) : "memory");
   VCMP_U16(18, v1, 0, 0, 0, 0, 0, 0x7548, 0x3489, 0x9388, 0x8188, 0x11ae,
            0x5808, 0x4891, 0x4902, 0x8759, 0x1111, 0x1989);
 
@@ -238,7 +238,7 @@ void TEST_CASE6(void) {
   VSET(3, e32, m1);
   VLOAD_32(v2, 4, 60, 4);
   asm volatile("csrs vstart, %0" ::"r"(2));
-  asm volatile("vloxei32.v v1, (%0), v2" ::"r"(&ALIGNED_I32[0]));
+  asm volatile("vloxei32.v v1, (%0), v2" ::"r"(&ALIGNED_I32[0]) : "memory");
   VCMP_U32(19, v1, 0, 0, 0xf9aa71f0);
 }
 
