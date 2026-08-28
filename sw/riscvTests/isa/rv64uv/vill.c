9a87ee321c3751d5c942325b8a3f50357ca02c01
Axel Vanoni <axvanoni@ethz.ch>
2026-08-28 11:00:21 +0200

    Fix test name and ensure vill is set properly

--- sw/riscvTests/isa/rv64uv/vill.c
+++ sw/riscvTests/isa/rv64uv/vill.c
@@ -1,12 +1,13 @@
 #include "vector_macros.h"
 
 int main(void) {
-
-  unsigned int vtype = 1 << 31;
+  unsigned int vlmul = 5;
+  unsigned int vsew  = 3;
+  unsigned int vtype = vsew << 3 | vlmul;
 
   __asm__ volatile (
       "li      t0, -1 \n"
-      "vsetvl zero, zero, t0 \n"
+      "vsetvl zero, zero, %[vtype] \n":: [vtype]"r"(vtype):
   );
 
   asm volatile("vadd.vv v24, v8, v16");
