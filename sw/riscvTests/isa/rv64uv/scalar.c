cc7a64e309e525eb0bcdc7bf01df051da38178c7
Navaneeth-Kunhi Purayil <nkunhi@iis.ee.ethz.ch>
2026-09-10 13:24:10 +0200

    sw: update tests

--- sw/riscvTests/isa/rv64uv/scalar.c
+++ sw/riscvTests/isa/rv64uv/scalar.c
@@ -3,22 +3,24 @@
 int main(void) {
 
   // ensure illegal vtype
-  unsigned int vlmul = 5;
-  unsigned int vsew  = 3;
-  unsigned int vtype = vsew << 3 | vlmul;
+  unsigned int vlmul = 4;
+  unsigned int illegal_vsew  = 4;
+  unsigned int vtype = illegal_vsew << 3 | vlmul;
 
   __asm__ volatile (
-      "li      t0, -1 \n"
       "vsetvl zero, zero, %[vtype] \n":: [vtype]"r"(vtype):
   );
 
+  // Scalar instructions should not be affected by the illegal vtype
   asm volatile("mul a0, a1, a2");
   asm volatile("add a0, a1, a2");
   asm volatile("div a0, a1, a2");
   asm volatile("fmul.b f0, f1, f2");
   asm volatile("fmul.h f0, f1, f2");
   asm volatile("fmul.s f0, f1, f2");
+#if ELEN == 64
   asm volatile("fmul.d f0, f1, f2");
+#endif
 
   return 0;
 }
--- sw/riscvTests/isa/rv64uv/vill.c
+++ sw/riscvTests/isa/rv64uv/vill.c
@@ -2,8 +2,13 @@
 
 int main(void) {
 
+  // ensure illegal vtype
+  unsigned int vlmul = 4;
+  unsigned int illegal_vsew  = 4;
+  unsigned int vtype = illegal_vsew << 3 | vlmul;
+
   __asm__ volatile (
-      "vsetvli zero, zero, e64, mf8, ta, ma\n"
+      "vsetvl zero, zero, %[vtype] \n":: [vtype]"r"(vtype):
   );
 
   asm volatile("vadd.vv v24, v8, v16");
