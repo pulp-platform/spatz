196c69aeffeb6be62c479715d27053f5a6bc8af0
Navaneeth-KunhiPurayil <100233443+Navaneeth-KunhiPurayil@users.noreply.github.com>
2026-09-21 09:23:09 +0200

    hw: fix vill check in spatz (#153)
    * Fix test name and ensure vill is set properly
    
    * Add new test for scalar instructions
    
    fdiv seems not to be implemented, so leave it out for now.
    
    * Bump spatz_vpu to version with scalar fixes
    
    * hw: check for exception from spatz in snitch
    
    * sw: update tests
    
    * hw: bump spatz_vpu
    
    * lint: fix trailing space
    
    * chore: bump spatz_vpu
    
    ---------
    
    Co-authored-by: Axel Vanoni <axvanoni@ethz.ch>

--- sw/riscvTests/isa/rv64uv/vill.c
+++ sw/riscvTests/isa/rv64uv/vill.c
@@ -2,11 +2,13 @@
 
 int main(void) {
 
-  unsigned int vtype = 1 << 31;
+  // ensure illegal vtype
+  unsigned int vlmul = 4;
+  unsigned int illegal_vsew  = 4;
+  unsigned int vtype = illegal_vsew << 3 | vlmul;
 
   __asm__ volatile (
-      "li      t0, -1 \n"
-      "vsetvl zero, zero, t0 \n"
+      "vsetvl zero, zero, %[vtype] \n":: [vtype]"r"(vtype):
   );
 
   asm volatile("vadd.vv v24, v8, v16");
